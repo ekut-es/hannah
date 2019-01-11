f5a8f4f1e6c45165c3fb75bc4081e2834bc7a048
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2019-01-11 12:25:07 +0100

    Added bootstrap.sh command

---
+++ bootstrap.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo yum install python36 python36-devel -y 
+sudo yum install freeglut-devel -y 
+sudo yum install portaudio-devel -y
+
+python3.6 -m ensurepip --user
+
+#install torch
+python3.6 -m pip install https://download.pytorch.org/whl/cpu/torch-1.0.0-cp36-cp36m-linux_x86_64.whl --user
+#python3.6 -m pip install torch --user
+python3.6 -m pip install torchvision --user
+
+
+#install tensorflow
+python3.6 -m pip install tensorflow --user
+#python3.6 -m pip install tensorflow-gpu --user
+
+
+python3.6 -m pip install --user chainmap
+python3.6 -m pip install --user cherrypy
+python3.6 -m pip install --user librosa
+python3.6 -m pip install --user Flask
+python3.6 -m pip install --user numpy
+python3.6 -m pip install --user Pillow
+python3.6 -m pip install --user PyAudio
+python3.6 -m pip install --user PyOpenGL
+python3.6 -m pip install --user PyOpenGL_accelerate
+python3.6 -m pip install --user pyttsx3
+python3.6 -m pip install --user requests
+python3.6 -m pip install --user SpeechRecognition
