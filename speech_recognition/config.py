ca9846e0729040339df83a1a2c3ed1918483de4a
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2019-02-08 14:36:19 +0100

    Add exporter

---
+++ speech_recognition/config.py
@@ -0,0 +1,31 @@
+from collections import ChainMap
+import argparse
+import sys
+
+class ConfigBuilder(object):
+    def __init__(self, *default_configs):
+        self.default_config = ChainMap(*default_configs)
+
+    def build_argparse(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--full-help", action="store_true")
+        for key, value in self.default_config.items():
+            key = "--{}".format(key)
+            if isinstance(value, tuple):
+                parser.add_argument(key, default=list(value), nargs=len(value), type=type(value[0]))
+            elif isinstance(value, list):
+                parser.add_argument(key, default=value, nargs="+", type=type(value[0]))
+            elif isinstance(value, bool) and not value:
+                parser.add_argument(key, action="store_true")
+            else:
+                parser.add_argument(key, default=value, type=type(value))
+        return parser
+
+    def config_from_argparse(self, parser=None):
+        if not parser:
+            parser = self.build_argparse()
+        args = vars(parser.parse_known_args()[0])
+        if args["full_help"]:
+            parser.print_help()
+            sys.exit(0)
+        return ChainMap(args, self.default_config)
--- speech_recognition/train.py
+++ speech_recognition/train.py
@@ -1,4 +1,5 @@
 from collections import ChainMap
+from config import ConfigBuilder
 import argparse
 import os
 import random
@@ -13,33 +14,6 @@ import torch.utils.data as data
 from . import models as mod
 from . import dataset
 
-class ConfigBuilder(object):
-    def __init__(self, *default_configs):
-        self.default_config = ChainMap(*default_configs)
-
-    def build_argparse(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--full-help", action="store_true")
-        for key, value in self.default_config.items():
-            key = "--{}".format(key)
-            if isinstance(value, tuple):
-                parser.add_argument(key, default=list(value), nargs=len(value), type=type(value[0]))
-            elif isinstance(value, list):
-                parser.add_argument(key, default=value, nargs="+", type=type(value[0]))
-            elif isinstance(value, bool) and not value:
-                parser.add_argument(key, action="store_true")
-            else:
-                parser.add_argument(key, default=value, type=type(value))
-        return parser
-
-    def config_from_argparse(self, parser=None):
-        if not parser:
-            parser = self.build_argparse()
-        args = vars(parser.parse_known_args()[0])
-        if args["full_help"]:
-            parser.print_help()
-            sys.exit(0)
-        return ChainMap(args, self.default_config)
 
 def print_eval(name, scores, labels, loss, end="\n"):
     batch_size = labels.size(0)
