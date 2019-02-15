8aec1aad8080e551e159aa7c1d458acc79befb8b
Christoph Gerum <gerum@informatik.uni-tuebingen.de>
2019-02-15 10:13:28 +0100

    Train all networks on gpu

--- train_all.sh
+++ train_all.sh
@@ -14,19 +14,19 @@
 
 
 
-python3.6 -m speech_recognition.train --data_folder datasets/speech_commands_v0.02/ --wanted_words yes no up down left right on off stop go --n_labels 12 --n_epochs 500 --model hello-ds-cnn-small --no_cuda --window_ms 40 --stride_ms 20 --n_dct 10
+python3.6 -m speech_recognition.train --data_folder datasets/speech_commands_v0.02/ --wanted_words yes no up down left right on off stop go --n_labels 12 --n_epochs 500 --model hello-ds-cnn-small --gpu_no 0 --window_ms 40 --stride_ms 20 --n_dct 10
 
-python3.6 -m speech_recognition.train --data_folder datasets/speech_commands_v0.02/ --wanted_words yes no up down left right on off stop go --n_labels 12 --n_epochs 500 --model hello-ds-cnn-medium --no_cuda --window_ms 40 --stride_ms 20 --n_dct 10
+python3.6 -m speech_recognition.train --data_folder datasets/speech_commands_v0.02/ --wanted_words yes no up down left right on off stop go --n_labels 12 --n_epochs 500 --model hello-ds-cnn-medium --gpu_no 0 --window_ms 40 --stride_ms 20 --n_dct 10
 
-python3.6 -m speech_recognition.train --data_folder datasets/speech_commands_v0.02/ --wanted_words yes no up down left right on off stop go --n_labels 12 --n_epochs 500 --model hello-ds-cnn-large --no_cuda --window_ms 40 --stride_ms 20 --n_dct 10 
-
-
+python3.6 -m speech_recognition.train --data_folder datasets/speech_commands_v0.02/ --wanted_words yes no up down left right on off stop go --n_labels 12 --n_epochs 500 --model hello-ds-cnn-large --gpu_no 0 --window_ms 40 --stride_ms 20 --n_dct 10 
 
 
-python3.6 -m speech_recognition.train --data_folder datasets/speech_commands_v0.02/ --wanted_words yes no up down left right on off stop go --n_labels 12 --n_epochs 500 --model hello-dnn-small --no_cuda --window_ms 40 --stride_ms 40 --n_dct 10
 
-python3.6 -m speech_recognition.train --data_folder datasets/speech_commands_v0.02/ --wanted_words yes no up down left right on off stop go --n_labels 12 --n_epochs 500 --model hello-dnn-medium --no_cuda --window_ms 40 --stride_ms 40 --n_dct 10
 
-python3.6 -m speech_recognition.train --data_folder datasets/speech_commands_v0.02/ --wanted_words yes no up down left right on off stop go --n_labels 12 --n_epochs 500 --model hello-dnn-large --no_cuda --window_ms 40 --stride_ms 40 --n_dct 10 
+python3.6 -m speech_recognition.train --data_folder datasets/speech_commands_v0.02/ --wanted_words yes no up down left right on off stop go --n_labels 12 --n_epochs 500 --model hello-dnn-small --gpu_no 0 --window_ms 40 --stride_ms 40 --n_dct 10
+
+python3.6 -m speech_recognition.train --data_folder datasets/speech_commands_v0.02/ --wanted_words yes no up down left right on off stop go --n_labels 12 --n_epochs 500 --model hello-dnn-medium --gpu_no 0 --window_ms 40 --stride_ms 40 --n_dct 10
+
+python3.6 -m speech_recognition.train --data_folder datasets/speech_commands_v0.02/ --wanted_words yes no up down left right on off stop go --n_labels 12 --n_epochs 500 --model hello-dnn-large --gpu_no 0 --window_ms 40 --stride_ms 40 --n_dct 10 
 
 
