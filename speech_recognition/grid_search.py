bc0b056c9530fc4f7477fce47ae42da4b5169736
Adrian Frischknecht <adrian.frischknecht@student.uni-tuebingen.de>
2019-07-19 14:33:15 +0200

    Updated grid search

--- speech_recognition/grid_search.py
+++ speech_recognition/grid_search.py
@@ -6,7 +6,7 @@
     - number of gpus is not considered
     - not all hyperparameters are considered
     - no summary is generated
-    - no visualization 
+    - no visualization
 """
 
 
@@ -15,26 +15,28 @@ import itertools
 
 def main():
     model_name, config = build_config()
-    n_epochs = 1
-    num_gpu = 4
+    n_epochs = 150
+    early_stopping = 25
+    num_gpu = 3
 
  #   print('Model name: ', model_name, ' Config: ', config)
 
     with open('grid_search.sh', mode='w+') as f:
         print('#!/bin/bash', file=f)
         for num_feature in range(10,40,10):
-            for feature, gpu in {('mfcc', 0), ('mel', 1), ('spectrogram', 2), ('melspec', 3)}:
-                for bottleneck in tuple(itertools.product(range(2), range(2))):
-                    for separable in tuple(itertools.product(range(2), range(2))):
-                        id='feature' + feature + 'numFeature' + str(num_feature) + 'bottle' + str(bottleneck[0]) + str(bottleneck[1]) + 'separable' + str(separable[0]) + str(separable[1])
+            for bottleneck in tuple(itertools.product(range(2), range(2))):
+                for separable in tuple(itertools.product(range(2), range(2))):
+                    for feature, gpu in {('mfcc', 3), ('mel', 3), ('spectrogram', 2), ('melspec', 2)}:
+                        #id='feature' + feature + 'numFeature' + str(num_feature) + 'bottle' + str(bottleneck[0]) + str(bottleneck[1]) + 'separable' + str(separable[0]) + str(separable[1])
+                        ex_id = 'evalLogger'
                         bottleneck_str = str(bottleneck).translate(str.maketrans("", "", ",()"))
                         separable_str = str(separable).translate(str.maketrans("", "", ",()"))
 
                         print('python3.6 -m speech_recognition.train '
-                              '--n_epochs {} '
-                              '--early-stopping 10 '
                               '--model {} '
                               '--experiment-id {} '
+                              '--n_epochs {} '
+                              '--early-stopping {}'
                               '--feature {} '
                               '--gpu_no {} '
                               '--n_mels {} '
@@ -42,10 +44,10 @@ def main():
                               '--bottleneck {} '
                               '--separable {} '
                               ' > /dev/null 2>&1 &'
-                              .format(n_epochs, model_name, id, feature, gpu, num_feature, num_feature, bottleneck_str, separable_str), file=f)
+                              .format(model_name, ex_id, n_epochs, early_stopping, feature, gpu, num_feature, num_feature, bottleneck_str, separable_str), file=f)
                         print('', file=f)
-            # Process multiple trainings per GPU at the same time
-            print('wait', file=f)
+                # Process multiple trainings per GPU at the same time
+                print('wait', file=f)
 
 if __name__ == "__main__":
     main()
