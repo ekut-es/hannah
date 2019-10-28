2eaa939cd4e100357d2b7b22139d39e01c3b979c
Adrian Frischknecht <frischkn@lucille.Informatik.Uni-Tuebingen.De>
2019-10-28 10:00:26 +0100

    Added vad dataset script

---
+++ datasets/get_datasets_vad.sh
@@ -0,0 +1,31 @@
+mkdir -p noise_files
+
+pushd noise_files
+wget https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-development.audio.{1,2,3,4,5,6,7,8,9,10}.zip
+unzip  -P pass '*.zip'
+
+rm  -f TUT-acoustic-scenes-2017-development.audio.1.zip
+rm  -f TUT-acoustic-scenes-2017-development.audio.2.zip
+rm  -f TUT-acoustic-scenes-2017-development.audio.3.zip
+rm  -f TUT-acoustic-scenes-2017-development.audio.4.zip
+rm  -f TUT-acoustic-scenes-2017-development.audio.5.zip
+rm  -f TUT-acoustic-scenes-2017-development.audio.6.zip
+rm  -f TUT-acoustic-scenes-2017-development.audio.7.zip
+rm  -f TUT-acoustic-scenes-2017-development.audio.8.zip
+rm  -f TUT-acoustic-scenes-2017-development.audio.9.zip
+rm  -f TUT-acoustic-scenes-2017-development.audio.10.zip
+
+popd
+
+mkdir -p speech_files
+
+pushd speech_files
+
+wget https://zeos.ling.washington.edu/corpora/UWNU/uwnu-v2.tar.gz
+tar xvzf  uwnu-v2.tar.gz
+rm -f uwnu-v2.tar.gz
+
+python split_vad_data_balanced.py
+
+popd
+
