57508f78d3f4d8eaea08c3782ec375f5356efdfa
Christoph Gerum <gerum@informatik.uni-tuebingen.de>
2019-12-14 03:06:52 +0100

    Make get datasets less verbose.

--- datasets/get_datasets.sh
+++ datasets/get_datasets.sh
@@ -3,7 +3,7 @@
 mkdir -p speech_commands_v0.02
 
 pushd speech_commands_v0.02
-wget  http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz
-tar xvzf  speech_commands_v0.02.tar.gz
+wget -q http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz
+tar xzf  speech_commands_v0.02.tar.gz
 rm -f speech_commands_v0.02.tar.gz
 popd 
