ed34023994cf8747d397b6c30ac0d466432d4cf0
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2020-04-23 10:26:26 +0200

    Remove pico_cnn backend
    Use onnx exporter from: https://github.com/ekut-es/pico-cnn for this
    functionality.

--- speech_recognition/export/backends/embedded_c/code_templates/pico_cnn_abs.c
+++
@@ -1,3 +0,0 @@
-for(int i = 0; i < {{input_buffer.typed_size}}; i++){
-  {{output_buffer.start_ptr}}[i] = fabsf({{input_buffer.start_ptr}}[i]);
-}
--- speech_recognition/export/backends/embedded_c/utils.py
+++
@@ -1,6 +0,0 @@
-from typing import Any, Text, Iterable, List, Dict, Sequence, Optional, Tuple, Union
-from functools import reduce
-
-
-def reduce_mult(data : Iterable[int]) -> int:
-    return reduce(lambda x, y: x * y, data, 1)
