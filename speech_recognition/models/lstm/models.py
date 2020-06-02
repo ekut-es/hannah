9b77d09b62dd5edbfcad5d320daeab6f40438a37
Paul Palomero Bernardo <paul.palomero-bernardo@uni-tuebingen.de>
2020-06-02 20:40:18 +0200

    Update LSTM

--- speech_recognition/models/lstm/models.py
+++ speech_recognition/models/lstm/models.py
@@ -16,12 +16,14 @@ class LSTMModel(SerializableModule):
             batch_first=True,
             dropout=config["dropout"],
         )
+        self.dropout = nn.Dropout(config["dropout"])
         self.fc = nn.Linear(config["hidden_size"], config["n_labels"])
 
     def forward(self, x):
         x = x.permute(0, 2, 1)
         _, (ht, _) = self.lstm(x)
-        x = self.fc(ht[-1])
+        x = self.dropout(ht[-1])
+        x = self.fc(x)
         return x
 
 
@@ -30,12 +32,12 @@ configs = {
         features="mel",
         hidden_size=64,
         n_layers=1,
-        dropout=0,
+        dropout=0.2,
         lr=0.001,
         optimizer="adam",
-        freq_min=80,
-        freq_max=7600,
-        n_mels=80,
+        freq_min=0,
+        freq_max=8000,
+        n_mels=32,
         n_mfcc=13,
     )
 }
