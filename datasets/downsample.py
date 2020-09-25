2ba80369cbdc5d8aa3460829dac0a57ab332a9b8
Tobias <thaldhs@gmail.com>
2020-09-25 20:40:33 +0200

    user output added

--- datasets/downsample.py
+++ datasets/downsample.py
@@ -1,5 +1,6 @@
 import os
 
+print("Start working")
 for path, subdirs, files in os.walk("./vad_data_balanced/"):
     for name in files:
         if name.endswith("wav") and not name.startswith("."):
@@ -9,5 +10,5 @@ for path, subdirs, files in os.walk("./vad_data_balanced/"):
         elif name.endswith("mp3") and not name.startswith("."):
             os.system("ffmpeg -y -i " + os.path.join(path, name) + " -ar 16000 -ac 1 -loglevel quiet " + os.path.join(path, name.replace(".mp3", ".wav")))
             os.system("rm " + os.path.join(path, name))
-
+print("Finished")
 
