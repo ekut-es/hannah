c4e05b799588df82fad3c9bcf693f0000fbe3cfb
Tobias <thaldhs@gmail.com>
2020-09-25 19:36:47 +0200

    wget problem of lucille

--- datasets/get_datasets_vad_extended.sh
+++ datasets/get_datasets_vad_extended.sh
@@ -5,8 +5,19 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
 
     mkdir -p noise_files
 
-    pushd noise_files
-    wget https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-development.audio.{1,2,3,4,5,6,7,8,9,10}.zip
+    cd noise_files
+    #wget https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-development.audio.{1,2,3,4,5,6,7,8,9,10}.zip
+    wget https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-development.audio.1.zip
+    wget https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-development.audio.2.zip
+    wget https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-development.audio.3.zip
+    wget https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-development.audio.4.zip
+    wget https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-development.audio.5.zip
+    wget https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-development.audio.6.zip
+    wget https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-development.audio.7.zip
+    wget https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-development.audio.8.zip
+    wget https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-development.audio.9.zip
+    wget https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-development.audio.10.zip
+
 
     unzip  -P pass '*.zip'
 
