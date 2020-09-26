62603299dcafed3e278698ca0ccd5c1f843a02e9
Tobias Hald <Tobias.Hald@student.uni-tuebingen.de>
2020-09-26 15:14:12 +0200

    commandline parameters added

--- datasets/get_dataset.py
+++ datasets/get_dataset.py
@@ -1,8 +1,21 @@
 import VADData
-
+import sys
 
 if __name__ == "__main__":
     print("test")
-    vad = VADData.VADData()
-    vad.get_small_dataset()
-    pass
+    print(sys.argv)
+    vad = None
+    kws = None
+    if "--vad_small" in sys.argv:
+        print("vad small will be generated")
+        vad = VADData.VADData()
+        vad.get_small_dataset()
+    if "--vad_big" in sys.argv:
+        print("vad big will be generated")
+        vad = VADData.VADData()
+        vad.get_big_dataset()
+    if "--vad_all" in sys.argv:
+        print("VAD will be generated")
+        vad = VADData.VADData()
+        vad.get_small_dataset()
+        vad.get_big_dataset()
