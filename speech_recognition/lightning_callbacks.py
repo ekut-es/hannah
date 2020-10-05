45326fbbb22a9be7e1c5e5cecb3c526cee683ab5
Christoph Gerum <christoph@gerum.de>
2020-10-05 16:39:09 +0200

    Add batch norm folding implementation

--- speech_recognition/lightning_callbacks.py
+++ speech_recognition/lightning_callbacks.py
@@ -3,11 +3,23 @@ import distiller
 
 
 class DistillerCallback(Callback):
+    def __init__(self, fold_bn=-1):
+        self.fold_bn = fold_bn
+        self.bn_frozen = False
+
     def on_train_start(self, trainer, pl_module):
         pl_module.model.to(pl_module.device)
         pl_module.msglogger.info("Activating compression scheduler")
+
+        optimizers = trainer.optimizers
+        if len(optimizers) != 1:
+            raise Exception(
+                "Distiller is only available when using  a single optimizers"
+            )
+        optimizer = optimizers[0]
+
         self.compression_scheduler = distiller.file_config(
-            pl_module.model, pl_module.optimizer, pl_module.hparams["compress"]
+            pl_module, optimizer, pl_module.hparams["compress"]
         )
 
     def on_epoch_start(self, trainer, pl_module):
@@ -32,14 +44,12 @@ class DistillerCallback(Callback):
             pl_module.batches_per_epoch,
         )
 
-        if self.hparams["fold_bn"] == self.current_epoch and not self.bn_frozen:
+        if self.fold_bn == trainer.current_epoch and not self.bn_frozen:
             self.bn_frozen = True
             self.msglogger.info("Freezing batch norms")
             # save_model(log_dir, model, test_set, config=config, model_prefix="before_freeze_")
 
             def freeze_func(model):
-                import distiller.quantization.sim_bn_fold
-
                 if isinstance(
                     model, distiller.quantization.sim_bn_fold.SimulatedFoldedBatchNorm
                 ):
@@ -49,7 +59,7 @@ class DistillerCallback(Callback):
                 self.model.apply(freeze_func)
 
             self.msglogger.info("Model after freezing")
-            self.msglogger.info(self.model)
+            self.msglogger.info(pl_module)
 
     def on_epoch_end(self, trainer, pl_module):
         self.compression_scheduler.on_epoch_end(pl_module.current_epoch)
