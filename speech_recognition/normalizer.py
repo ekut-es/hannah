a2a3e966bc03ed6a49d3749bd54a6ccb727afced
Christoph Gerum <christoph@gerum.de>
2020-11-10 18:44:48 +0100

    Fix normalizer

--- speech_recognition/normalizer.py
+++ speech_recognition/normalizer.py
@@ -5,12 +5,12 @@ class FixedPointNormalizer(nn.Module):
     "Simple feature normalizer for fixed point models"
 
     def __init__(self, normalize_bits: int = 8, normalize_max: int = 256):
+        super().__init__()
         self.normalize_bits = normalize_bits
         self.normalize_max = normalize_max
 
     def forward(self, x):
         normalize_factor = 2.0 ** (self.normalize_bits - 1)
-
         x = x * normalize_factor / self.normalize_max
         x = x.round()
         x = x / normalize_factor
