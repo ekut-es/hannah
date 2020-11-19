387ffb6fb50f738f9a3c651941e47e0b38229447
Tobias <thaldhs@gmail.com>
2020-11-19 21:20:32 +0100

    read UWNU and Mozilla with its division

--- speech_recognition/datasets/DatasetSplit.py
+++ speech_recognition/datasets/DatasetSplit.py
@@ -3,21 +3,15 @@ import random
 import shutil
 import numpy as np
 from ..utils import list_all_files, extract_from_download_cache
+from .NoiseDataset import NoiseDataset
+from torchvision.datasets.utils import list_dir
+from pandas import DataFrame
+import pandas as pd
 
 
 class DatasetSplit:
     def __init__(self):
         pass
-
-    @classmethod
-    def load_vad_default(cls, speechdir, noisedir):
-        noise_files = list_all_files(noisedir, ".wav", True, ".")
-        noise_files.extend(list_all_files(noisedir, ".mp3", True, "."))
-
-        speech_files = list_all_files(speechdir, ".wav", True, ".")
-        speech_files.extend(list_all_files(speechdir, ".mp3", True, "."))
-
-        return (speech_files, noise_files)
 
     @classmethod
     def vad(cls, config):
@@ -26,8 +20,11 @@ class DatasetSplit:
         noise_dir = os.path.join(data_folder, "noise_files")
         speech_dir = os.path.join(data_folder, "speech_files")
 
+        noise_files = NoiseDataset.getTUT_NoiseFiles(config)
+        noise_files_others = NoiseDataset.getOthers_divided(config)
+
         # list all noise  and speech files
-        speech_files, noise_files = DatasetSplit.load_vad_default(speech_dir, noise_dir)
+        speech_files = DatasetSplit.read_UWNU(config)
 
         # randomly shuffle the noise and speech files and split them in train,
         # validation and test set
@@ -46,9 +43,25 @@ class DatasetSplit:
         dev_noise = noise_files[nb_train_noise : nb_train_noise + nb_dev_noise]
         test_noise = noise_files[nb_train_noise + nb_dev_noise :]
 
+        train_other, dev_other, test_other = noise_files_others
+        train_noise.extend(train_other)
+        dev_noise.extend(dev_other)
+        test_noise.extend(test_other)
+
+        nb_noise_files += len(test_other)
+        nb_train_noise += len(train_other)
+        nb_dev_noise += len(dev_other)
+
         train_speech = speech_files[:nb_train_speech]
         dev_speech = speech_files[nb_train_speech : nb_train_speech + nb_dev_speech]
         test_speech = speech_files[nb_train_speech + nb_dev_speech :]
+
+        mozilla = DatasetSplit.read_mozilla(config)
+        mozilla_train, mozilla_dev, mozilla_test = mozilla
+
+        train_speech.extend(mozilla_train)
+        dev_speech.extend(mozilla_dev)
+        test_speech.extend(mozilla_test)
 
         destination_dict = {
             "train/noise": train_noise,
@@ -68,7 +81,10 @@ class DatasetSplit:
         noise_dir = os.path.join(data_folder, "noise_files")
         speech_dir = os.path.join(data_folder, "speech_files")
 
-        speech_files, noise_files = DatasetSplit.load_vad_default(speech_dir, noise_dir)
+        noise_files = NoiseDataset.getTUT_NoiseFiles(config)
+        noise_files_others = NoiseDataset.getOthers_divided(config)
+
+        speech_files = DatasetSplit.read_UWNU(config)
 
         # randomly shuffle noise and speech files and split them in train,
         # validation and test set
@@ -83,9 +99,29 @@ class DatasetSplit:
         dev_noise = noise_files[nb_train_noise : nb_train_noise + nb_dev_noise]
         test_noise = noise_files[nb_train_noise + nb_dev_noise :]
 
+        train_other, dev_other, test_other = noise_files_others
+        train_noise.extend(train_other)
+        dev_noise.extend(dev_other)
+        test_noise.extend(test_other)
+
         train_speech = speech_files[:nb_train_noise]
         dev_speech = speech_files[nb_train_noise : nb_train_noise + nb_dev_noise]
         test_speech = speech_files[nb_train_noise + nb_dev_noise : nb_noise_files]
+
+        mozilla = DatasetSplit.read_mozilla(config)
+        mozilla_train, mozilla_dev, mozilla_test = mozilla
+
+        random.shuffle(mozilla_train)
+        random.shuffle(mozilla_dev)
+        random.shuffle(mozilla_test)
+
+        train_speech.extend(mozilla_train[0 : len(train_other)])
+        dev_speech.extend(mozilla_dev[0 : len(dev_other)])
+        test_speech.extend(mozilla_test[0 : len(test_other)])
+
+        random.shuffle(train_noise)
+        random.shuffle(dev_noise)
+        random.shuffle(test_noise)
 
         train_bg_noise = train_noise[:100]
         dev_bg_noise = dev_noise[:100]
@@ -111,6 +147,7 @@ class DatasetSplit:
         data_folder = config["data_folder"]
         noise_dir = os.path.join(data_folder, "noise_files")
         speech_dir = os.path.join(data_folder, "speech_files")
+        speech_dir = os.path.join(speech_dir, "uwnu-v2")
 
         # list all noise  and speech files
         noise_files = list_all_files(noise_dir, ".wav", True)
@@ -169,8 +206,8 @@ class DatasetSplit:
         speech_dir = os.path.join(data_folder, "speech_commands_v0.02")
 
         # list all noise  and speech files
-        noise_files = list_all_files(noise_dir, ".wav", True)
-        noise_files.extend(list_all_files(noise_dir, ".mp3", True))
+        noise_files = NoiseDataset.getTUT_NoiseFiles(config)
+        noise_files_others = NoiseDataset.getOthers_divided(config)
 
         speech_files = []
         for path, subdirs, files in os.walk(speech_dir):
@@ -191,6 +228,15 @@ class DatasetSplit:
         train_noise = noise_files[:nb_train_noise]
         dev_noise = noise_files[nb_train_noise : nb_train_noise + nb_dev_noise]
         test_noise = noise_files[nb_train_noise + nb_dev_noise :]
+
+        train_other, dev_other, test_other = noise_files_others
+        train_noise.extend(train_other)
+        dev_noise.extend(dev_other)
+        test_noise.extend(test_other)
+
+        nb_noise_files += len(test_other)
+        nb_train_noise += len(train_other)
+        nb_dev_noise += len(dev_other)
 
         train_speech = speech_files[:nb_train_noise]
         dev_speech = speech_files[nb_train_noise : nb_train_noise + nb_dev_noise]
@@ -258,3 +304,39 @@ class DatasetSplit:
                     oldpath = os.path.join(data_folder, name)
                     if os.path.isdir(oldpath):
                         shutil.rmtree(oldpath)
+
+    @classmethod
+    def read_UWNU(cls, config):
+        data_folder = config["data_folder"]
+        speech_folder = os.path.join(data_folder, "speech_files")
+        uwnu_folder = os.path.join(speech_folder, "uwnu-v2")
+
+        if os.path.isdir(uwnu_folder):
+
+            output = list_all_files(uwnu_folder, ".wav", True, ".")
+            output.extend(list_all_files(uwnu_folder, ".mp3", True, "."))
+
+            return output
+        return []
+
+    @classmethod
+    def read_mozilla(cls, config):
+        data_folder = config["data_folder"]
+        speech_folder = os.path.join(data_folder, "speech_files")
+        mozilla_folder = os.path.join(speech_folder, "cv-corpus-5.1-2020-06-22")
+
+        if os.path.isdir(mozilla_folder):
+            output = [[], [], []]
+            files = ["train.tsv", "dev.tsv", "test.tsv"]
+
+            lang_folders = list_dir(mozilla_folder, prefix=True)
+            for lang in lang_folders:
+                for idx, file in enumerate(files):
+                    path = os.path.join(lang, file)
+                    tmp_csv = pd.read_csv(path, sep="\t")
+                    clips = os.path.join(lang, "clips")
+                    clips = os.path.join(clips, "")
+                    output[idx].extend(clips + tmp_csv.path[:])
+
+            return (output[0], output[1], output[2])
+        return ([], [], [])
