86928c3192c040fc4a903278554d87cc17ae3091
Felix Lorenz <felix.lorenz@student.uni-tuebingen.de>
2020-12-21 12:28:34 +0100

    major minor blocks wip

--- speech_recognition/models/nas/tc_model.py
+++ speech_recognition/models/nas/tc_model.py
@@ -4,9 +4,15 @@ import torch
 from torch.autograd import Variable
 from ..utils import SerializableModule, next_power_of2
 from hydra.utils import instantiate
-from collections import OrderedDict
 
 msglogger = logging.getLogger()
+
+
+def create_act(act, clipping_value):
+    if act == "relu":
+        return nn.ReLU()
+    else:
+        return nn.Hardtanh(0.0, clipping_value)
 
 
 class ApproximateGlobalAveragePooling1D(nn.Module):
@@ -23,54 +29,165 @@ class ApproximateGlobalAveragePooling1D(nn.Module):
         return x
 
 
-# max. two MajorBlocks in parallel
-class TCResidualBlock(nn.Module):
-    pass
-
-
-# Sequence of MicroBlocks
-# + (optional: activation) + (optional: batch normalization) + (optional: dropout)
+# TODO: implement case: block type input and minor type parallel
 class MajorBlock(nn.Module):
     def __init__(
-        self, output_channels, stride, branch, minor_blocks, input_channels=None
+        self,
+        output_channels,
+        stride,
+        branch,
+        minor_blocks,
+        input_channels=None,
+        act_layer=None,
     ):
         super().__init__()
-        # Example of using Sequential with OrderedDict
-        # model = nn.Sequential(OrderedDict([
-        #           ('conv1', nn.Conv2d(1,20,5)),
-        #           ('relu1', nn.ReLU()),
-        #           ('conv2', nn.Conv2d(20,64,5)),
-        #           ('relu2', nn.ReLU())
-        #         ]))
-        self.main_modules = []
-        self.parallel_modules = []
 
-        # TODO: implement case: block type input and minor type parallel
-        for n, minor_block in enumerate(minor_blocks):
-            print(f"!!! minor_block = {minor_block}")
+        assert act_layer is not None
 
-            if minor_block["parallel"]:
-                self.parallel_modules.append(
-                    instantiate(minor_block, input_channels=input_channels)
+        # module lists for branches of MajorBlock
+        self.main_modules = nn.ModuleList()
+        self.parallel_modules = nn.ModuleList()
+
+        self.is_residual_block = False
+        if branch == "residual":
+            self.is_residual_block = True
+
+        # config for iteration of minors
+        n_parallels = sum(block["parallel"] for block in minor_blocks)
+        n_mains = len(minor_blocks) - n_parallels
+        count_main = 1
+        count_parallel = 1
+
+        stride_main = stride
+        stride_parallel = stride
+
+        input_channels_main = input_channels
+        input_channels_parallel = input_channels
+
+        kwargs_main = {
+            "batch_norm": True,
+            "act_layer": act_layer,
+        }
+
+        # BUILD MajorBlock of MinorBlocks
+        for minor_block in minor_blocks:
+
+            is_parallel = minor_block["parallel"]
+            size = minor_block["size"]
+
+            # parallel MinorBlocks
+            if is_parallel:
+
+                if count_parallel > 1:
+                    input_channels_parallel = output_channels
+                    stride_parallel = 1
+
+                module = MinorBlock(
+                    input_channels_parallel,
+                    output_channels,
+                    size,
+                    stride_parallel,
+                    batch_norm=True,
+                    act_layer=act_layer,
                 )
+
+                count_parallel += 1
+                self.parallel_modules.append(module)
+
+            # main MinorBlocks
             else:
-                self.main_modules.append(
-                    instantiate(minor_block, input_channels=input_channels)
+                if count_main > 1:
+                    input_channels_main = output_channels
+                    stride_main = 1
+                    kwargs_main
+
+                if not self.is_residual_block:
+                    kwargs_main = {
+                        "batch_norm": False,
+                        "act_layer": None,
+                    }
+                elif count_main == n_mains:
+                    kwargs_main = {
+                        "batch_norm": True,
+                        "act_layer": None,
+                    }
+
+                module = MinorBlock(
+                    input_channels_main,
+                    output_channels,
+                    size,
+                    stride_main,
+                    **kwargs_main
                 )
+
+                self.main_modules.append(module)
+                count_main += 1
+
+        # only append activation layer if residual block
+        if self.is_residual_block:
+            self.act_layer = act_layer
 
     def forward(self, x):
-        y = self.main(x)
-        if self.parallel > 1:
-            x = self.parallel(x)
 
-        res = self.act(y + x)
+        main_feed = x
+        parallel_feed = x
 
-        return res
+        # always feed through main modules
+        for layer in self.main_modules:
+            main_feed = layer(main_feed)
+
+        if self.is_residual_block:
+
+            for layer in self.parallel_modules:
+                parallel_feed = layer(parallel_feed)
+
+            act_input = main_feed + parallel_feed
+
+            output = self.act_layer(act_input)
+
+        output = main_feed
+
+        return output
 
 
 # 1D-Conv + (optional: activation) + (optional: batch normalization) + (optional: dropout)
 class MinorBlock(nn.Module):
-    pass
+    def __init__(
+        self,
+        input_channels,
+        output_channels,
+        size,
+        stride,
+        batch_norm=True,
+        act_layer=None,
+    ):
+        super().__init__()
+        layers = []
+
+        layers.append(
+            nn.Conv1d(
+                input_channels,
+                output_channels,
+                size,
+                stride,
+                # padding=dilation * pad_x,
+                # dilation=dilation,
+                bias=False,
+            )
+        )
+
+        if batch_norm:
+            layers.append(nn.BatchNorm1d(output_channels))
+
+        if act_layer is not None:
+            layers.append(act_layer)
+
+        self.sequential = nn.Sequential(*layers)
+
+    def forward(self, x):
+        y = self.sequential(x)
+
+        return y
 
 
 class TCCandidateModel(SerializableModule):
@@ -89,6 +206,7 @@ class TCCandidateModel(SerializableModule):
         dilation_factor = config["dilation_factor"]
         clipping_value = config["clipping_value"]
         small = config["small"]
+        act_layer = create_act(activation_fnc, clipping_value)
 
         # TODO: make following properties configurable
         bottleneck = 0
@@ -100,15 +218,18 @@ class TCCandidateModel(SerializableModule):
         major_blocks = config["major_blocks"]
 
         # model
-        self.modules = nn.ModuleList()
+        self.modules = []
 
         # BUILD MODEL BODY
         input_channels = height
 
         for major_block in major_blocks:
             # first major block cannot be parallel
-            current_module = instantiate(major_block, input_channels=input_channels)
+            current_module = instantiate(
+                major_block, input_channels=input_channels, act_layer=act_layer
+            )
             self.modules.append(current_module)
+            input_channels = major_block["output_channels"]
 
         # ------------------
 
@@ -118,8 +239,11 @@ class TCCandidateModel(SerializableModule):
         x = Variable(torch.zeros(1, height, width))
 
         # iterate over the layers of the main branch to get dummy output
+        print("!!! TCCandidateModel layers:")
         for layer in self.modules:
+            print(layer)
             x = layer(x)
+        print("------------------------------")
 
         # average pooling
         shape = x.shape
