7c01fcc4260b29fd207605c530b5539f57402c99
Tobias <tobias.hald@student.uni-tuebingen.de>
2021-01-31 16:14:44 +0100

    new loss functions techer free in give 2 versions

--- speech_recognition/modules/distilling_classifier.py
+++ speech_recognition/modules/distilling_classifier.py
@@ -2,6 +2,7 @@ from .classifier import SpeechClassifierModule
 from omegaconf import DictConfig
 from typing import Optional
 import torch.nn as nn
+import torch
 from pytorch_lightning.callbacks import ModelCheckpoint
 from typing import Union
 
@@ -41,6 +42,67 @@ class SpeechKDClassifierModule(SpeechClassifierModule):
             self.loss_func = None
 
         print(f"!!! teacher model is {teacher_model} with type {type(teacher_model)}")
+
+    """
+    Code taken from Paper: "KD-Lib: A PyTorch library for Knowledge Distillation, Pruning and Quantization"
+    arxiv: 2011.14691
+    License: MIT
+
+    Original idea coverd in: "Revisit Knowledge Distillation: a Teacher-free Framework"
+    arxiv: 1909.11723
+    """
+
+    def teacher_free_framework_loss(
+        self, y_pred_student, y_true, distil_weight=0.5, correct_prob=0.9, temp=10.0
+    ):
+        local_loss = nn.KLDivLoss()
+        num_classes = y_pred_student.shape[1]
+
+        soft_label = torch.ones_like(
+            y_pred_student
+        )  # .to(self.device) Brauchen wir das?
+        soft_label = soft_label * (1 - correct_prob) / (num_classes - 1)
+
+        for i in range(y_pred_student.shape[0]):
+            soft_label[i, y_true[i]] = correct_prob
+
+        loss = (1 - distil_weight) * torch.nn.functional.cross_entropy(
+            y_pred_student, y_true
+        )
+        loss += (distil_weight) * local_loss(
+            torch.nn.functional.log_softmax(y_pred_student, dim=1),
+            torch.nn.functional.softmax(soft_label / temp, dim=1),
+        )
+        return loss
+
+    """
+    Code taken from Paper: "KD-Lib: A PyTorch library for Knowledge Distillation, Pruning and Quantization"
+    arxiv: 2011.14691
+    License: MIT
+
+    Original idea coverd in: "Revisit Knowledge Distillation: a Teacher-free Framework"
+    arxiv: 1909.11723
+    """
+
+    def teacher_free_selfkd_loss(
+        self, y_pred_student, y_pred_teacher, y_true, distil_weight=0.5
+    ):
+        """
+        Function used for calculating the KD loss during distillation
+
+        :param y_pred_student (torch.FloatTensor): Prediction made by the student model
+        :param y_pred_teacher (torch.FloatTensor): Prediction made by the teacher model
+        :param y_true (torch.FloatTensor): Original label
+        """
+        local_loss = nn.KLDivLoss()
+        loss = (1 - distil_weight) * torch.nn.functional.cross_entropy(
+            y_pred_student, y_true
+        )
+        loss += (distil_weight) * local_loss(
+            torch.nn.functional.log_softmax(y_pred_student, dim=1),
+            torch.nn.functional.softmax(y_pred_teacher / self.temp, dim=1),
+        )
+        return loss
 
     def training_step(self, batch, batch_idx):
         # x inputs, y labels
