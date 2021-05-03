2d22ec7f0f9216495a0f6e6a27a281a746dbc6be
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2021-05-03 08:54:47 +0200

    Merge masterthesis_kraussmo

--- speech_recognition/callbacks/optimization.py
+++ speech_recognition/callbacks/optimization.py
@@ -7,6 +7,19 @@ class HydraOptCallback(Callback):
     def __init__(self, monitor=["val_loss"]):
         self.monitor = monitor
         self.values = {}
+        self.tests = {}
+
+    def on_test_end(self, trainer, pl_module):
+        callback_metrics = trainer.callback_metrics
+
+        for telement in [
+            "test_accuracy",
+            "test_error",
+            "test_recall",
+            "test_precision",
+            "rest_f1",
+        ]:
+            self.tests[telement] = callback_metrics[telement]
 
     def on_validation_end(self, trainer, pl_module):
         callback_metrics = trainer.callback_metrics
@@ -14,6 +27,9 @@ class HydraOptCallback(Callback):
         for monitor in self.monitor:
             if monitor in callback_metrics:
                 self.values[monitor] = callback_metrics[monitor]
+
+    def test_result(self):
+        return self.tests
 
     def result(self):
 
