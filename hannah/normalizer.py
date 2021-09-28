5dfe445c5c0239c0947682c7e89aa83bc9b965d7
Hald <tobias.hald@student.uni-tuebingen.de>
2021-09-28 17:30:36 +0200

    changes

--- hannah/normalizer.py
+++ hannah/normalizer.py
@@ -27,17 +27,12 @@ class AdaptiveFixedPointNormalizer(nn.Module):
 
     def __init__(self, normalize_bits: int = 8, normalize_max: int = 256):
         super().__init__()
-        self.normalize_bits = normalize_bits
-        self.normalize_max = normalize_max
+        self.bn = nn.BatchNorm1d(num_features=40, affine=False)
 
     def forward(self, x):
-        normalize_factor = 2.0 ** (self.normalize_bits - 1)
-        x = x * normalize_factor / self.normalize_max
-        x = x.round()
-        x = x / normalize_factor
-        x = x.clamp(-1.0, 1.0 - 1.0 / normalize_factor)
-
-        return x
+        # print(torch.max(self.bn.running_mean))
+        # print(torch.max(self.bn.running_var))
+        return self.bn(x)
 
 
 class HistogramNormalizer(nn.Module):
