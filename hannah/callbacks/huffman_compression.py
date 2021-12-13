930f8a094c05e3def13aea8ac3e122e9891bb759
wernerju <julia-helga.werner@student.uni-tuebingen.de>
2021-12-13 17:32:05 +0100

    fixed cuda error

--- hannah/callbacks/huffman_compression.py
+++ hannah/callbacks/huffman_compression.py
@@ -14,32 +14,61 @@ class CompressionHuff(Callback):
 
 
     def on_epoch_end(self, trainer, pl_module):
-        if trainer.current_epoch == self.compress_after-2:
+        if trainer.current_epoch == self.compress_after-1:
             with torch.no_grad():
                 for name, module in pl_module.named_modules():
                     if hasattr(module, "scaled_weight"):
-                        module.weight.data = module.scaled_weight 
-            
+                        module.weight.data = module.scaled_weight
+
 
             def replace_modules(module):
                 for name, child in module.named_children():
                     replace_modules(child)
 
                     if isinstance(child, ConvBn1d):
-                        print(name)
-                        setattr(module, name, Conv1d(child.in_channels, child.out_channels, child.kernel_size, qconfig=child.qconfig))
+                        setattr(module, name, 
+                        Conv1d(
+                        child.in_channels,
+                        child.out_channels,
+                        child.kernel_size, 
+                        stride=child.stride,
+                        padding=child.padding,
+                        groups=child.groups,
+                        padding_mode=child.padding_mode,
+                        bias=child.bias,
+                        dilation=child.dilation,
+                        qconfig=child.qconfig,
+                        out_quant=True))
 
                     if isinstance(child, ConvBnReLU1d):
-                        print(name)
-                        setattr(module, name, ConvReLU1d(child.in_channels, child.out_channels, child.kernel_size, qconfig=child.qconfig))
+                        setattr(module, name, 
+                        ConvReLU1d(
+                        child.in_channels,
+                        child.out_channels,
+                        child.kernel_size, 
+                        stride=child.stride,
+                        padding=child.padding,
+                        groups=child.groups,
+                        padding_mode=child.padding_mode,
+                        bias=child.bias,
+                        dilation=child.dilation,
+                        qconfig=child.qconfig,
+                        out_quant=True))
 
+            device = pl_module.device
             replace_modules(pl_module)
-            #pl_module.cuda()
-            ws = []
-            #for name, module in pl_module.named_parameters():
-            #    ws = np.append(ws, module.data.cpu().detach().numpy())
+            pl_module.to(device=device) # otherwise cuda error
             #print(pl_module)
-            #frq = Counter(ws.tolist())
-            #print(frq)
-            #print(len(frq))
+
+
+            # get frequencies
+            ws = []
+            for name, module in pl_module.named_parameters():
+                #print(name)
+                #print(module)
+                ws = np.append(ws, module.cpu().detach().numpy())
+            frq = Counter(ws.tolist())
+            print(frq)
+            print(len(frq))
+
             print('##############')
