9734c105cf2cb2630412e510334360da90414232
wernerju <julia-helga.werner@student.uni-tuebingen.de>
2022-01-29 09:39:11 +0100

    minor changes

--- hannah/callbacks/compression_kmeans_test.py
+++ hannah/callbacks/compression_kmeans_test.py
@@ -32,103 +32,97 @@ class CompressionHuff(Callback):
 
     def on_train_end(self, trainer, pl_module):
         
-        if True: #if trainer.current_epoch == self.compress_after-5:  #if trainer.current_epoch % 3 == 0: 
-            with torch.no_grad():
-                counter = 0
-                for name, module in pl_module.named_modules():
-                    if hasattr(module, "scaled_weight"):
-                        module.weight.data = module.scaled_weight
-                        if not isinstance(module, nn.Linear):
-                            bias_shape = [1] * len(module.weight.shape)
-                            bias_shape[1] = -1
-                            bias = torch.zeros(module.out_channels, device=module.weight.device)
-                            bias = module.bias_fake_quant((bias - module.bn.running_mean) * module.scale_factor + module.bn.bias) #.reshape(bias_shape) #.view(-1, 1, 1) #.reshape(bias_shape)
-                            module.bias = torch.nn.Parameter(bias)
+        with torch.no_grad():
+            for module in pl_module.modules():
+                if hasattr(module, "scaled_weight"):
+                    module.weight.data = module.scaled_weight
+                    if not isinstance(module, nn.Linear):
+                        bias_shape = [1] * len(module.weight.shape)
+                        bias_shape[1] = -1
+                        bias = torch.zeros(module.out_channels, device=module.weight.device)
+                        bias = module.bias_fake_quant((bias - module.bn.running_mean) * module.scale_factor + module.bn.bias) #.reshape(bias_shape) #.view(-1, 1, 1) #.reshape(bias_shape)
+                        module.bias = torch.nn.Parameter(bias)
     
 
-            def replace_modules(module):
-                for name, child in module.named_children():
-                    replace_modules(child)
+        def replace_modules(module):
+            for name, child in module.named_children():
+                replace_modules(child)
 
-                    if isinstance(child, ConvBn1d):
-                        tmp = Conv1d(
-                        child.in_channels,
-                        child.out_channels,
-                        child.kernel_size, 
-                        stride=child.stride,
-                        padding=child.padding,
-                        groups=child.groups,
-                        padding_mode=child.padding_mode,
-                        dilation=child.dilation,
-                        bias=True,
-                        qconfig=child.qconfig
-                        )
-                        tmp.weight.data = child.weight
-                        tmp.bias = child.bias
-                        setattr(module, name, tmp)
-                        #getattr(module,name, tmp).bias = child.bias
-
+                if isinstance(child, ConvBn1d):
+                    tmp = Conv1d(
+                    child.in_channels,
+                    child.out_channels,
+                    child.kernel_size, 
+                    stride=child.stride,
+                    padding=child.padding,
+                    groups=child.groups,
+                    padding_mode=child.padding_mode,
+                    dilation=child.dilation,
+                    bias=True,
+                    qconfig=child.qconfig
+                    )
+                    tmp.weight.data = child.weight
+                    tmp.bias = child.bias
+                    setattr(module, name, tmp)
 
 
-                    if isinstance(child, ConvBnReLU1d):
-                        tmp = ConvReLU1d(
-                        child.in_channels,
-                        child.out_channels,
-                        child.kernel_size, 
-                        stride=child.stride,
-                        padding=child.padding,
-                        groups=child.groups,
-                        padding_mode=child.padding_mode,
-                        bias=True,
-                        dilation=child.dilation,
-                        qconfig=child.qconfig)
-                        tmp.weight.data = child.weight
-                        tmp.bias = child.bias
-                        setattr(module, name, tmp)
-                        #getattr(module,name, tmp).bias = child.bias
-                        #getattr(module,name, tmp).weight.data = child.weight
+                if isinstance(child, ConvBnReLU1d):
+                    tmp = ConvReLU1d(
+                    child.in_channels,
+                    child.out_channels,
+                    child.kernel_size, 
+                    stride=child.stride,
+                    padding=child.padding,
+                    groups=child.groups,
+                    padding_mode=child.padding_mode,
+                    bias=True,
+                    dilation=child.dilation,
+                    qconfig=child.qconfig)
+                    tmp.weight.data = child.weight
+                    tmp.bias = child.bias
+                    setattr(module, name, tmp)
                         
 
-            device = pl_module.device
-            replace_modules(pl_module)
-            pl_module.to(device=device) # otherwise cuda error
-
-            device = pl_module.device
-            for module in pl_module.modules():
-                if hasattr(module, "weight") and module.weight != None:
-                    params = module.weight.data.cpu().numpy().flatten()
-                    centers = clustering(params)
-
-                    # Returns center that is closest to given value x
-                    def replace_values_by_centers(x):
-                        i = (np.abs(centers - x)).argmin() 
-                        return centers[i] 
-                    module.weight.data = module.weight.data.cpu().apply_(replace_values_by_centers) #_ symbolizes inplace function, tensor moved to cpu, since apply_() only works that way
-                    module.to(device=device) # move from cpu to gpu
-                    #centers = np.unique(module.weight.data.cpu().numpy().flatten(), return_counts=False)
-                    #print(centers)
+        device = pl_module.device
+        replace_modules(pl_module)
+        pl_module.to(device=device) # otherwise cuda error
 
 
-            # Perform KMeans clustering again on all center coordinates
-            '''m = csr_matrix(c) 
-            min_value = min(m.data)
-            max_value = max(m.data)
-            range_cluster = np.linspace(min_value, max_value, num=18)
-            kmeans = KMeans(n_clusters=len(range_cluster), init=range_cluster.reshape(-1,1), n_init=1, algorithm="full")
-            kmeans.fit(m.data.reshape(-1,1))
-            c = kmeans.cluster_centers_.reshape(-1)
-            def replace_values_by_centers(x):
-                i = (np.abs(np.asarray(c) - x)).argmin() 
-                return c[i] 
-            for module in pl_module.modules():
-                if hasattr(module, "weight") and module.weight != None:
-                    module.weight.data = module.weight.data.cpu().apply_(replace_values_by_centers) #_ symbolizes inplace function, tensor moved to cpu, since apply_() only works that way
-                    module.to(device=device)'''
+        for module in pl_module.modules():
+            if hasattr(module, "weight") and module.weight != None:
+                params = module.weight.data.cpu().numpy().flatten()
+                centers = clustering(params)
+
+                # Returns center that is closest to given value x
+                def replace_values_by_centers(x):
+                    i = (np.abs(centers - x)).argmin() 
+                    return centers[i] 
+                module.weight.data = module.weight.data.cpu().apply_(replace_values_by_centers) #_ symbolizes inplace function, tensor moved to cpu, since apply_() only works that way
+                module.to(device=device) # move from cpu to gpu
+                #centers = np.unique(module.weight.data.cpu().numpy().flatten(), return_counts=False)
+                #print(centers)
+
+
+        # Perform KMeans clustering again on all center coordinates
+        '''m = csr_matrix(c) 
+        min_value = min(m.data)
+        max_value = max(m.data)
+        range_cluster = np.linspace(min_value, max_value, num=18)
+        kmeans = KMeans(n_clusters=len(range_cluster), init=range_cluster.reshape(-1,1), n_init=1, algorithm="full")
+        kmeans.fit(m.data.reshape(-1,1))
+        c = kmeans.cluster_centers_.reshape(-1)
+        def replace_values_by_centers(x):
+            i = (np.abs(np.asarray(c) - x)).argmin() 
+            return c[i] 
+        for module in pl_module.modules():
+            if hasattr(module, "weight") and module.weight != None:
+                module.weight.data = module.weight.data.cpu().apply_(replace_values_by_centers) #_ symbolizes inplace function, tensor moved to cpu, since apply_() only works that way
+                module.to(device=device)'''
 
 
     def on_epoch_end(self, trainer, pl_module):
-        print(trainer.callback_metrics['val_accuracy'].item())
-        if trainer.current_epoch % 2 == 0: #if (trainer.callback_metrics['val_accuracy'].item() > 0.92) and (trainer.current_epoch % 3 == 0):
+        print('Training validation accuracy: ', trainer.callback_metrics['val_accuracy'].item())
+        if trainer.current_epoch % 2 == 0: #if (trainer.callback_metrics['val_accuracy'].item() > 0.92):
             print('Clustering.')
             device = pl_module.device
             for module in pl_module.modules():
@@ -137,9 +131,9 @@ class CompressionHuff(Callback):
                     centers = clustering(w)
                     def replace_values_by_centers(x):
                         i = (np.abs(centers - x)).argmin() 
-                        return centers[i] # returns center that is closest to given value x
-                    module.weight.data = module.weight.data.cpu().apply_(replace_values_by_centers) #_ symbolizes inplace function, tensor moved to cpu, since apply_() only works that way
-                    module.to(device=device) # move from cpu to gpu
+                        return centers[i] 
+                    module.weight.data = module.weight.data.cpu().apply_(replace_values_by_centers) 
+                    module.to(device=device) 
 
 
                 
