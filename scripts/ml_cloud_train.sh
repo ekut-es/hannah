b39454180e7060bd1455a3084e16398ac1c36365
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2022-01-31 11:55:35 +0100

    Add ml_cloud training scripts

---
+++ scripts/ml_cloud_train.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+####
+#a) Define slurm job parameters
+####
+
+#SBATCH --job-name=ml_cloud_train
+
+#resources:
+
+#SBATCH --cpus-per-task=4
+
+#SBATCH --partition=gpu-2080ti-preemptable
+# the slurm partition the job is queued to.
+
+#SBATCH --nodes=1
+# requests that the cores are all on one node
+
+#SBATCH --mem=8G
+# the job will need 64GB of memory equally distributed on 4 cpus.
+
+#SBATCH --gres=gpu:rtx2080ti:1
+#the job can use and see 1 GPUs (8 GPUs are available in total on one node)
+
+#SBATCH --gres-flags=enforce-binding
+
+#SBATCH --time=4320
+# the maximum time the scripts needs to run in minutes
+
+#SBATCH --error=job_%j.err
+# write the error output to job.*jobID*.err
+
+#TSBATCH --output/home/bringmann/cgerum05/job_%j.out
+#SBATCH --output=job_%j.out
+# write the standard output to your home directory job.*jobID*.out
+
+#SBATCH --mail-type=ALL
+#write a mail if a job begins, ends, fails, gets requeued or stages out
+
+#SBATCH --mail-user=christoph.gerum@uni-tuebingen.de
+# your mail address
+
+
+#Script
+echo "Job information"
+scontrol show job $SLURM_JOB_ID
+
+#echo "Copy training data"
+
+
+echo "Moving datasets to local scratch ${SCRATCH} ${SLURM_JOB_ID}"
+
+cp /home/bringmann/cgerum05/ml_cloud.simg $SCRATCH
+cp -r hannah $SCRATCH
+
+echo "Running training with config $1"
+date
+export HANNAH_CACHE_DIR=$SCRATCH/cache
+cd $SCRATCH
+singularity run --no-home --bind $PWD --bind $WORK:$WORK:rw $SCRATCH/ml_cloud.sif python -m hannah.train dataset.data_folder=$SCRATCH/datasets
+date
+
+echo DONE!
