a12f2deb53787afc69e48ceef6b80698c061875b
wernerju <julia-helga.werner@student.uni-tuebingen.de>
2022-02-07 07:36:35 +0100

    minor changes

--- hannah/compression.py
+++ hannah/compression.py
@@ -10,7 +10,7 @@ def load_parameters(file_path):
     model = onnx.load(os.path.dirname(__file__) + file_path)
     parameters = []
     for i in model.graph.initializer:
-        if "weight" in i.name:
+        if "weight" in i.name: # for conv_net_trax, add other case for non_quantized models
             param = numpy_helper.to_array(i)
             parameters.append(param.flatten())
     return parameters
@@ -21,12 +21,14 @@ def replace_cluster_by_indices(parameters):
     ws = copy.deepcopy(parameters)
     cluster = 10  # number of clusters
     ws_indexed = []
-    # index_LUT = np.full(shape=(len(length_layers), cluster+1), fill_value=0) 
+    print(len(parameters[0]))
+    index_LUT = np.full(shape=(len(ws), cluster+1), fill_value=0) 
+    print(index_LUT)
     for k in range(len(ws)):
         centers = np.unique(ws[k], return_counts=False)  # get unique cluster centers
         # print(len(centers))
-        #for j in range(len(centers)):  # fill LUT
-        #    index_LUT[i,j] = centers_layer[j]
+        '''for j in range(len(centers)):  # fill LUT
+            index_LUT[k,j] = centers[j]'''
         intermediate_layer = np.select([ws[k]==centers[j] for j in range(len(centers))], 
                     [i+1 for i in range(len(centers))], 
                     ws[k])  # replace with index 1 to i for i cluster
@@ -37,9 +39,14 @@ def replace_cluster_by_indices(parameters):
 def main():
     file_path = '/../trained_models/test/tc-res8/model.onnx'
     parameters = load_parameters(file_path) 
+
+    print('----------- Replacement of Clusters by indices -------------')
     ws_indexed = replace_cluster_by_indices(parameters)
+
     print('----------- Huffman Encoding -------------')
     hs, tree = Huffman_encoding(ws_indexed)
+
+    print('----------- Huffman Decoding -------------')
     decoding = Huffman_decoding(hs, tree)
     print('Norm of decoded weights and indexed weights: ', (np.linalg.norm(torch.FloatTensor(decoding)==ws_indexed))) # check if indexed ws and decoded sequence are equal
     
