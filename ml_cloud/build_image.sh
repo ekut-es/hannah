3009c93fecadce66c0dbbbe72ed5c59d1bda0be1
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2022-02-14 16:57:33 +0100

    Add dependencies for OpenPCDET

--- ml_cloud/build_image.sh
+++ ml_cloud/build_image.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 poetry export --without-hashes | grep -v hannah-optimizer > requirements.txt
-singularity build  ml_cloud.sif  ml_cloud.recipe
+singularity build  --fakeroot ml_cloud.sif  ml_cloud.recipe
