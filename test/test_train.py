b284e184248be1ad94e2db615249ca0ec106ce0e
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2022-03-07 12:07:16 +0100

    Fix ofa test parametrization

--- test/test_train.py
+++ test/test_train.py
@@ -57,9 +57,9 @@ def test_models(model, features):
         ("ofa", "False", "5"),
     ],
 )
-def test_ofa(model, evaluate_random, random_evaluate_number):
+def test_ofa(model, random_evaluate, random_evaluate_number):
     epochs = 1
-    command_line = f"python -m hannah.train --config-name nas_ofa trainer.overfit_batches=5 experiment_id=test_ofa nas.epochs_warmup={epochs} nas.epochs_kernel_step={epochs} nas.epochs_depth_step={epochs} nas.epochs_dilation_step={epochs} nas.epochs_width_step={epochs} nas.random_evaluate=False model={model} nas.random_evaluate=True nas.random_eval_number=10"
+    command_line = f"python -m hannah.train --config-name nas_ofa trainer.overfit_batches=5 experiment_id=test_ofa nas.epochs_warmup={epochs} nas.epochs_kernel_step={epochs} nas.epochs_depth_step={epochs} nas.epochs_dilation_step={epochs} nas.epochs_width_step={epochs} nas.random_evaluate=False model={model} nas.random_evaluate={random_evaluate} nas.random_eval_number={random_evaluate_number}"
     subprocess.run(command_line, shell=True, check=True, cwd=topdir)
 
 
