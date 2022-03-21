d3a9dc1ed29487e91d4a4b207aa5ee530763a455
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-03-21 10:48:47 +0100

    add parametrization

--- hannah/nas/search_space/tcresnet/tcresnet_space.py
+++ hannah/nas/search_space/tcresnet/tcresnet_space.py
@@ -1,51 +1,18 @@
 from hannah.nas.search_space.symbolic_space import Space
-from hannah.nas.search_space.symbolic_operator import Context
+from hannah.nas.search_space.symbolic_operator import Context, Choice, Variable, infer_in_channel, infer_padding
 import hydra
 from omegaconf import DictConfig
 from hydra.utils import instantiate
 import hannah.conf  # noqa
 from hannah.modules.config_utils import get_model
-import torch.nn as nn
 import torch
 import numpy as np
 from hannah.nas.search_space.torch_converter import TorchConverter
-
-
-class MethodWrapper(nn.Module):
-    def __init__(self, method, args, kwargs) -> None:
-        super().__init__()
-        # self.name = name
-        self.method = method
-        self.args = args
-        self.kwargs = kwargs
-
-    def forward(self, x):
-        xargs = []
-        if isinstance(x, list):
-            x, *xargs = x
-        return getattr(x, self.method)(*xargs, *self.args, **self.kwargs)
-
-
-class FunctionWrapper(nn.Module):
-    def __init__(self, function, args=[], kwargs={}) -> None:
-        super().__init__()
-        # self.name = name
-        self.function = function
-        self.args = args
-        self.kwargs = kwargs
-
-    def forward(self, x):
-        if isinstance(x, list):
-            return self.function(*x, *self.args, **self.kwargs)
-        else:
-            return self.function(x, *self.args, **self.kwargs)
-
-    def __repr__(self):
-        return "Wrapped({})".format(self.function)
+from hannah.nas.search_space.utils import get_first_cfg
 
 
 class TCResNetSpace(Space):
-    def __init__(self, config):
+    def __init__(self, config, parameterization=None):
         super().__init__()
         model_config = instantiate(config.model)
         self.model = get_model(model_config)
@@ -53,15 +20,33 @@ class TCResNetSpace(Space):
         converter = TorchConverter()
         converter.convert_model_to_space(self.model, self)
 
+        if parameterization:
+            for symop in self.nodes:
+                attrs = {}
+                if 'kernel_size' in symop.params:
+                    attrs['kernel_size'] = Choice('kernel_size', 1, 3, 5)
+                if 'padding' in symop.params:
+                    attrs['padding'] = Variable('padding', func=infer_padding)
+                if 'stride' in symop.params:
+                    attrs['stride'] = Choice('stride', 1)
+                if 'in_channels' in symop.params:
+                    attrs['in_channels'] = Variable('in_channels', func=infer_in_channel)
+                if 'out_channels' in symop.params:
+                    attrs['out_channels'] = Choice('out_channels', *range(8, 128, 8))
+                if 'num_features' in symop.params:  # and "BatchNorm" in str(symop.target_cls):
+                    attrs['num_features'] = Variable('num_features', func=infer_in_channel)
+                if 'in_features' in symop.params:  # and "BatchNorm" in str(symop.target_cls):
+                    attrs['in_features'] = Variable('in_features', func=infer_in_channel)
 
-@hydra.main(config_name="config", config_path="../../../conf")
-def main(config: DictConfig):
+                symop.update_parameters(**attrs)
+
+
+def test_basic_model(config):
     space = TCResNetSpace(config)
     ctx = Context(None)
     x = torch.ones([1, 40, 101])
     space.model.eval()
     out_model = space.model(x)
-    print("Out Model", out_model)
     instance, _ = space.infer_parameters(x, ctx)
     state_dict = {}
     for key, value in space.model.state_dict().items():
@@ -70,8 +55,44 @@ def main(config: DictConfig):
     instance.load_state_dict(state_dict)
     instance.eval()
     out_instance = instance(x)
-    print("Out Instance", out_instance)
     np.testing.assert_allclose(out_model.detach(), out_instance.detach())
+
+
+@hydra.main(config_name="config", config_path="../../../conf")
+def main(config: DictConfig):
+    # test_basic_model(config)
+
+    space = TCResNetSpace(config, parameterization=True)
+    cfg_dims = space.get_config_dims()
+    # cfg = get_random_cfg(cfg_dims)
+    cfg = get_first_cfg(cfg_dims)
+    # cfg = {k: {k_: 0 if k_ == 'stride' else v_ for k_, v_ in v.items()} for k, v in cfg.items()}
+    ctx = Context(cfg)
+    x = torch.ones([1, 40, 101])
+    instance, out = space.infer_parameters(x, ctx, verbose=True)
+    print(instance)
+    print(out.shape)
+
+    # keys = list(flatten_config(cfg).keys())
+    # df = pd.DataFrame(columns=keys + ['valid'])
+
+    # for i in range(500000):
+    #     if i % 1000 == 0:
+    #         print('{}|{}'.format(i, 500000))
+    #     cfg = get_random_cfg(cfg_dims)
+    #     flat_data = flatten_config(cfg)
+    #     try:
+    #         ctx = Context(cfg)
+    #         x = torch.ones([1, 40, 101])
+    #         instance, out = space.infer_parameters(x, ctx, verbose=False)
+    #         valid = 1
+    #     except Exception as e:
+    #         valid = 0
+    #     flat_data['valid'] = valid
+    #     df = df.append(flat_data, ignore_index=True)
+    # if i % 10000 == 0:
+    #     df.to_csv('/home/moritz/projects/hannah/hannah/nas/search_space/tcresnet/data.csv')
+    # df.to_csv('/home/moritz/projects/hannah/hannah/nas/search_space/tcresnet/data.csv')
 
 
 if __name__ == '__main__':
