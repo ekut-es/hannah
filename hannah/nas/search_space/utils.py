eedcf2e39e2a5008417a2f61b4e388ac0c940070
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-03-21 10:49:32 +0100

    add config utils

--- hannah/nas/search_space/utils.py
+++ hannah/nas/search_space/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import itertools
 
 
 def get_same_padding(kernel_size):
@@ -40,3 +41,52 @@ def get_random_cfg(cfg_dims):
         else:
             cfg[k] = int(np.random.choice(v))
     return cfg
+
+
+def get_first_cfg(cfg_dims):
+    cfg = {}
+    for k, v in cfg_dims.items():
+        if isinstance(v, dict):
+            cfg[k] = {}
+            for k_, v_ in v.items():
+                if isinstance(v_, list):
+                    cfg[k][k_] = v_[0]
+                elif isinstance(v_, dict) and 'min' in v_ and 'size':
+                    # TODO: Change
+                    raise NotImplementedError()
+                    # cfg[k][k_] = np.random.uniform(v_['min'], v_['max'], v_['size'])
+                    cfg[k][k_] = torch.FloatTensor(v_['size']).uniform_(v_['min'], v_['max'])
+                elif isinstance(v_, dict):
+                    key = list(v_.keys())[0]
+                    cfg[k][k_] = key
+                    cfg[key] = {}
+                    for k__, v__ in v_[key].items():
+                        cfg[key][k__] = v__[0]
+
+        else:
+            cfg[k] = int(np.random.choice(v))
+    return cfg
+
+
+def flatten_config(config):
+    flatten_cfg = {}
+    for k, v in config.items():
+        for k_, v_ in v.items():
+            flatten_cfg[k + '.' + k_] = v_
+    return flatten_cfg
+
+
+def unflatten_config(config):
+    nested_cfg = {}
+    for k, v in config.items():
+        modkey, parkey = k.split('.')
+        if modkey not in nested_cfg:
+            nested_cfg[modkey] = {}
+        nested_cfg[modkey][parkey] = v
+    return nested_cfg
+
+
+def generate_config_permutations(config_dims):
+    keys, values = zip(*config_dims.items())
+    for v in itertools.product(*values):
+        yield unflatten_config(dict(zip(keys, v)))
