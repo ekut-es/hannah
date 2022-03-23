90ce53491b1681839400d9630fa529b605488de3
Joud Zakharia <joud.zakharia@student.uni-tuebingen.de>
2022-03-23 12:16:08 +0100

    Clean Ups

--- hannah/modules/image_classifier.py
+++ hannah/modules/image_classifier.py
@@ -5,7 +5,7 @@ import torch.utils.data as data
 from torchmetrics.functional import accuracy
 from torchmetrics.functional import precision
 from torchmetrics.functional import recall
-from torchmetrics.functional import f1
+from torchmetrics.functional import f1_score
 from timm.data.mixup import Mixup
 from hydra.utils import instantiate, get_class
 from torchmetrics import ConfusionMatrix
@@ -81,6 +81,10 @@ class ImageClassifierModule(ClassifierModule):
     def training_step(self, batch, batch_idx):
         x, y = batch
 
+        mixup_args = self.hparams.dataset.augmentations.mixup_args
+        mixup_fn = Mixup(**mixup_args)
+        x, y = mixup_fn(x, y)
+
         if batch_idx == 0:
             loggers = self._logger_iterator()
             for logger in loggers:
@@ -89,13 +93,8 @@ class ImageClassifierModule(ClassifierModule):
 
                     images = torchvision.utils.make_grid(x, normalize=True)
                     logger.experiment.add_image(f"input{batch_idx}", images)
-        if self.hparams.dataset.use_augmentations.mixup_args:
-            mixup_args = self.hparams.dataset.augmentations.mixup_args
-            mixup_fn = Mixup(**mixup_args)
-            x, y = mixup_fn(x, y)
-            logits = self(x)
-        else:
-            logits = self(x)
+
+        logits = self(x)
 
         if self.hparams.dataset.weighted_loss:
             loss = F.cross_entropy(
@@ -120,12 +119,12 @@ class ImageClassifierModule(ClassifierModule):
         # y = y.to("cpu")
         precision_micro = precision(preds, y)
         recall_micro = recall(preds, y)
-        f1_micro = f1(preds, y)
+        f1_micro = f1_score(preds, y)
         precision_macro = precision(
             preds, y, num_classes=self.num_classes, average="macro"
         )
         recall_macro = recall(preds, y, num_classes=self.num_classes, average="macro")
-        f1_macro = f1(preds, y, num_classes=self.num_classes, average="macro")
+        f1_macro = f1_score(preds, y, num_classes=self.num_classes, average="macro")
 
         self.log("val_loss", loss)
         self.log("val_error", 1 - acc, sync_dist=True)
@@ -148,17 +147,16 @@ class ImageClassifierModule(ClassifierModule):
 
         with set_deterministic(False):
             self.test_confusion(preds, y)
-        # preds = preds.to("cpu")
-        # y = y.to("cpu")
 
         precision_micro = precision(preds, y)
         recall_micro = recall(preds, y)
-        f1_micro = f1(preds, y)
+        f1_micro = f1_score(preds, y)
         precision_macro = precision(
             preds, y, num_classes=self.num_classes, average="macro"
         )
         recall_macro = recall(preds, y, num_classes=self.num_classes, average="macro")
-        f1_macro = f1(preds, y, num_classes=self.num_classes, average="macro")
+        f1_macro = f1_score(preds, y, num_classes=self.num_classes, average="macro")
+
         self.log("test_loss", loss, sync_dist=True)
         self.log("test_error", 1 - acc, sync_dist=True)
         self.log("test_accuracy", acc, sync_dist=True)
