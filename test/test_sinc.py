2cc36ec1fe5baa0b1d24f4d4138b36328d97d69f
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2022-03-24 11:33:28 +0100

    Add test for sinc convolutions

---
+++ test/test_sinc.py
@@ -0,0 +1,34 @@
+import copy
+
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+
+from hannah.features import SincConv
+
+
+def test_sinc():
+    model = SincConv(padding=50)
+    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+
+    orig_parameters = {}
+    for name, parameter in model.named_parameters():
+        orig_parameters[name] = copy.deepcopy(parameter.detach())
+
+    for epoch in range(20):
+
+        input = torch.rand(16, 1, 1600)
+        target = torch.rand(16, 40, 1600)
+        optimizer.zero_grad()
+        output = model(input)
+        print(output.shape)
+        loss = F.mse_loss(output, target)
+        loss.backward()
+        optimizer.step()
+
+    for name, parameter in model.named_parameters():
+        assert not torch.equal(parameter, orig_parameters[name])
+
+
+if __name__ == "__main__":
+    test_sinc()
