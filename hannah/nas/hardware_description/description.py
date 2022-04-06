f8d4c6f9788c2473d96f6213d4d52b2b8365651d
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2022-04-06 15:01:25 +0200

    Add optional op

--- hannah/nas/hardware_description/description.py
+++ hannah/nas/hardware_description/description.py
@@ -11,11 +11,11 @@ from .axis_type import AxisType
 from .optional_op import OptionalOp
 
 
-def int(signed: bool = True, bits: int = 8):
+def int_t(signed: bool = True, bits: int = 8):
     return IntType(signed=signed, bits=bits)
 
 
-def float(signed=True, significand_bits=23, exponent_bits=8):
+def float_t(signed=True, significand_bits=23, exponent_bits=8):
     return FloatType(
         signed=signed, significand_bits=significand_bits, exponent_bits=exponent_bits
     )
@@ -25,6 +25,10 @@ def axis(
     name: str, size: Optional[int] = None, compression: Optional[CompressionType] = None
 ):
     return AxisType(name=name, size=size, compression=compression)
+
+
+def memory(size: Optional[int] = None, name: Optional[str] = ""):
+    return MemoryType(size=size, name=name)
 
 
 def quantization(
@@ -50,3 +54,65 @@ def op(name, *operands, **attributes):
 
 def optional(op: OpType):
     return OptionalOp(op)
+
+
+if __name__ == "__main__":
+    # UltraTrail Description
+
+    ## DataTypes
+    weight_type = int_t(bits=6)
+    bias_type = int_t(bits=8)
+    activation_type = int_t(bits=8)
+    accumulator_type = int_t(bits=20)
+
+    ## Memories
+    fmem1 = memory(size=2 ** 16)
+    fmem2 = memory(size=2 ** 16)
+    fmem3 = memory(size=2 ** 16)
+    lmem = memory(size=2 ** 16)
+    bmem = memory(size=2 ** 16)
+
+    ## Tensors
+    feature_tensor = tensor(
+        (axis("N", 1), axis("C"), axis("W"), axis("c", 8)),
+        dtype=activation_type,
+        quantization=quantization(None, scale=1 / 2 ** 7, zero_point=0.0),
+    )
+
+    weight_tensor = tensor(
+        (axis("O"), axis("I"), axis("W"), axis("o", 8), axis("i", 8)),
+        dtype=weight_type,
+        quantization=quantization(None, scale=1 / 2 ** 5, zero_point=0.0),
+    )
+
+    linear_weight_tensor = tensor(
+        (axis("O"), axis("I"), axis("o", 8), axis("i", 8)),
+        dtype=weight_type,
+        quantization=quantization(None, scale=1 / 2 ** 7, zero_point=0.0),
+    )
+
+    bias_tensor = tensor(
+        (axis("N", 1), axis("C"), axis("c", 8)),
+        dtype=bias_type,
+        quantization=quantization(None, scale=1 / 2 ** 7, zero_point=0.0),
+    )
+
+    accumulator_tensor = tensor(
+        (axis("N", 1), axis("C"), axis("W"), axis("c", 8)),
+        dtype=accumulator_type,
+        quantization=quantization(None, scale=1 / 2 ** 14, zero_point=0.0),
+    )
+
+    conv = op("conv", feature_tensor, weight_tensor, out_type=accumulator_tensor)
+    # linear = op("linear", feature_tensor, linear_weight_tensor, out_type=accumulator_tensor)
+    # bias_add = optional(op("add", choice(conv, linear), bias_tensor, out_type=accumulator_tensor))
+    bias_add = optional(op("add", conv, bias_tensor, out_type=accumulator_tensor))
+    residual_add = optional(
+        op("add", bias_add, accumulator_tensor, out_dtype=accumulator_type)
+    )
+    relu = optional(op("relu", residual_add, out_dtype=accumulator_type))
+    requantize = op("requantize", relu, out_type=feature_tensor)
+
+    from devtools import debug
+
+    debug(requantize)
---
+++ hannah/nas/hardware_description/optional_op.py
@@ -0,0 +1,3 @@
+class OptionalOp:
+    def __init__(self, op):
+        self.op = op
