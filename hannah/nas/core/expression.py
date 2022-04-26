e444dc4e9c5a8fb372c78c8dc77b717cb6dc8f42
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2022-04-26 14:31:31 +0200

    Add core expression

---
+++ hannah/nas/core/expression.py
@@ -0,0 +1,100 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class Expression(ABC):
+    @abstractmethod
+    def evaluate(self):
+        ...
+
+    @abstractmethod
+    def format(self, indent=2, length=80) -> str:
+        ...
+
+    def __str__(self) -> str:
+        return self.format()
+
+    def __lt__(self, other):
+        from ..expressions.conditions import LTCondition
+
+        return LTCondition(self, other)
+
+    def __le__(self, other):
+        from ..expressions.conditions import LECondition
+
+        return LECondition(self, other)
+
+    def __gt__(self, other):
+        from ..expressions.conditions import GTCondition
+
+        return GTCondition(self, other)
+
+    def __ge__(self, other):
+        from ..expressions.conditions import GECondition
+
+        return GECondition(self, other)
+
+    def __eq__(self, other):
+        from ..expressions.conditions import EQCondition
+
+        return EQCondition(self, other)
+
+    def __ne__(self, other):
+        from ..expressions.conditions import NECondition
+
+        return NECondition(self, other)
+
+    def __add__(self, other):
+        from ..expressions.arithmetic import Add
+
+        return Add(self, other)
+
+    def __sub__(self, other):
+        from ..expressions.arithmetic import Sub
+
+        return Sub(self, other)
+
+    def __mul__(self, other):
+        from ..expressions.arithmetic import Mul
+
+        return Mul(self, other)
+
+    def __truediv__(self, other):
+        from ..expressions.arithmetic import Truediv
+
+        return Truediv(self, other)
+
+    def __floordiv__(self, other):
+        from ..expressions.arithmetic import Floordiv
+
+        return Floordiv(self, other)
+
+    def __mod__(self, other):
+        from ..expressions.arithmetic import Mod
+
+        return Mod(self, other)
+
+    def __divmod__(self, other):
+        raise NotImplementedError()
+
+    def __pow__(self, other):
+        raise NotImplementedError()
+
+    def __lshift__(self, other):
+        raise NotImplementedError()
+
+    def __rshift__(self, other):
+        raise NotImplementedError()
+
+    def __and__(self, other):
+        from ..expressions.logic import And
+
+        return And(self, other)
+
+    def __xor__(self, other):
+        raise NotImplementedError()
+
+    def __or__(self, other):
+        from ..expressions.logic import Or
+
+        return Or(self, other)
