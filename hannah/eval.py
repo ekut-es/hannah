8485881eeb9d98f0f4e37a72ae29452be3d8c5e2
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2022-05-02 05:09:56 +0200

    Hannah eval for dsd nas

--- hannah/eval.py
+++ hannah/eval.py
@@ -1,17 +1,15 @@
 import logging
-
 from pathlib import Path
 
-import hydra
+import tabulate
 import torch
-
-
-from hydra.utils import to_absolute_path, instantiate
+from hydra.utils import instantiate, to_absolute_path
 from omegaconf import DictConfig
 from pytorch_lightning import Trainer
 from pytorch_lightning.utilities.seed import reset_seed, seed_everything
 
 import hannah.modules.classifier
+import hydra
 
 
 def eval_checkpoint(config: DictConfig, checkpoint):
@@ -31,10 +29,26 @@ def eval_checkpoint(config: DictConfig, checkpoint):
     module.setup("test")
     module.load_state_dict(checkpoint["state_dict"])
 
-    trainer = Trainer(gpus=0, deterministic=True)
-    trainer.validate(model=module, ckpt_path=None)
-    reset_seed()
-    trainer.test(model=module, ckpt_path=None)
+    trainer = Trainer(gpus=1, deterministic=True)
+    # reset_seed()
+    # trainer.validate(model=module, ckpt_path=None)
+    # reset_seed()
+    # trainer.test(model=module, ckpt_path=None)
+
+    snr_values = []
+    for test_snr in [-5.0, 0, 5, 10, 15, 20, 25, 30, 35, 40]:
+        hparams["dataset"]["test_snr"] = test_snr
+        hparams["num_workers"] = 8
+        module = instantiate(hparams, _recursive_=False)
+        module.setup("test")
+        module.load_state_dict(checkpoint["state_dict"])
+
+        reset_seed()
+        trainer.test(model=module, ckpt_path=None)
+        metric = module.test_metrics["test_accuracy"].compute()
+        snr_values.append((test_snr, metric))
+
+    print(tabulate.tabulate(snr_values, headers=["SNR", "Accuracy"]))
 
 
 def eval(config: DictConfig):
