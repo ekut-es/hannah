2655502f831bdb2da45a46f53d6efab80bf485e8
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-05-11 09:39:59 +0200

    add Generic base class

--- hannah/nas/core/expression.py
+++ hannah/nas/core/expression.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, Generic, TypeVar
 
+T = TypeVar('T')
 
-class Expression(ABC):
+class Expression(ABC, Generic[T]):
     @abstractmethod
     def evaluate(self):
         ...
