fc29fb851fa07887c91051ac1d5ceb549c001efd
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-05-11 10:16:59 +0200

    extend ultratrail device def

--- hannah/nas/hardware_description/device.py
+++ hannah/nas/hardware_description/device.py
@@ -1,11 +1,11 @@
 from typing import List
 from hannah.nas.dataflow.dataflow_graph import DataFlowGraph
+from hannah.nas.hardware_description.memory_type import MemoryType
 from hannah.nas.parameters import parametrize
 from abc import ABC, abstractmethod
 
+
 class Device(ABC):
-
-
     def ops(self) -> List[DataFlowGraph]:
         ...
 
@@ -15,3 +15,15 @@ class Device(ABC):
 
 @parametrize
 class Ultratrail(Device):
+    def __init__(self,
+                 weight_bits: int = 8,
+                 bias_bits: int = 8,
+                 activation_bits: int = 8,
+                 accumulator_bits: int = 8,
+                 max_weight_bits: int = 8) -> None:
+        super().__init__()
+        self.weight_bits = weight_bits
+        self.bias_bits = bias_bits
+        self.activation_bits = activation_bits
+        self.accumulator_bits = accumulator_bits
+        self.max_weight_bits = max_weight_bits
