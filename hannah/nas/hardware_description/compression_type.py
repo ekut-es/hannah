c9351eb1521a496e5aafb364d44f16d920cf8e04
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-05-11 09:43:04 +0200

    moved to dataflow

--- hannah/nas/hardware_description/compression_type.py
+++
@@ -1,3 +0,0 @@
-class CompressionType:
-    def __init__(self, method: str = "rle") -> None:
-        self.method = method
--- hannah/nas/hardware_description/op_type.py
+++
@@ -1,5 +0,0 @@
-class OpType:
-    def __init__(self, name, *operands, **attributes):
-        self.name = name
-        self.operands = operands
-        self.attributes = attributes
