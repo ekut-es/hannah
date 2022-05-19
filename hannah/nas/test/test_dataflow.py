f55d4c49d1925f79555980fd112a72d983db7f37
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-05-19 18:10:54 +0200

    [WIP] add printing tests for debugging

--- hannah/nas/test/test_dataflow.py
+++ hannah/nas/test/test_dataflow.py
@@ -1,57 +1,69 @@
-from typing import Optional, Tuple
-
-from torch import Tensor
-from hannah.nas.dataflow.axis_type import AxisType
-from hannah.nas.dataflow.compression_type import CompressionType
-from hannah.nas.dataflow.data_type import DataType, FloatType
-from hannah.nas.dataflow.dataflow_graph import dataflow
+from hannah.nas.dataflow.dataflow_graph import dataflow, DataFlowGraph
 from hannah.nas.dataflow.op_type import OpType
-from hannah.nas.dataflow.quantization_type import QuantizationType
 from hannah.nas.dataflow.tensor_type import TensorType
-from hannah.nas.hardware_description.memory_type import MemoryType
-from hannah.nas.parameters import IntScalarParameter
-from hannah.nas.parameters.parameters import CategoricalParameter
+from hannah.nas.parameters.parameters import CategoricalParameter, FloatScalarParameter, IntScalarParameter
+from hannah.nas.ops import axis, tensor, batched_image_tensor, float_t
+
+
+@dataflow
+def conv_relu(input: TensorType,
+              output_channel=IntScalarParameter(4, 64),
+              kernel_size=CategoricalParameter([1, 3, 5]),
+              stride=CategoricalParameter([1, 2])):
+
+    weight = tensor((axis('o', size=output_channel),
+                     axis('i', size=input.output_tensor().axis['c'].size),
+                     axis('kh', size=kernel_size),
+                     axis('kw', size=kernel_size)),
+                    dtype=float_t(),
+                    name='weight')
+
+    op = OpType('conv2d', input, weight, stride=stride)
+    relu = OpType('relu', op)
+    return relu
+
+
+@dataflow
+def block(input: TensorType,
+          expansion=FloatScalarParameter(1, 6),
+          output_channel=IntScalarParameter(4, 64),
+          kernel_size=CategoricalParameter([1, 3, 5]),
+          stride=CategoricalParameter([1, 2])):
+
+    out = conv_relu(input, output_channel*expansion, kernel_size, stride)
+    out = conv_relu(out, output_channel, 1, 1)
+    return out
 
 
 def test_dataflow():
-    def axis(name: str, size: Optional[int] = None, compression: Optional[CompressionType] = None):
-        return AxisType(name=name, size=size, compression=compression)
-
-
-    def tensor(axis: Tuple[AxisType, ...],
-               dtype: DataType = FloatType(),
-               quantization: Optional[QuantizationType] = None,
-               memory: Optional[MemoryType] = None):
-        return TensorType(axis=axis, dtype=dtype, quantization=quantization, memory=memory)
-
-    @dataflow
-    def conv(input: TensorType,
-             output_channel=IntScalarParameter(4, 64),
-             kernel_size=CategoricalParameter([1, 3, 5]),
-             stride=CategoricalParameter([1, 2])):
-
-        weight = tensor((axis('o', size=output_channel),
-                        axis('i', size=input.axis['c'].size),
-                        axis('kh', size=kernel_size),
-                        axis('kw', size=kernel_size)))
-        return OpType('conv2d', input, weight, stride=stride)
-
-    @dataflow
-    def conv_block():
-        conv
-        conv
-        conv
-
-
-    input = tensor((axis('n'),
-                    axis('c'),
-                    axis('h'),
-                    axis('w')))
-
-    out = conv(input)
+    input = batched_image_tensor(name='input')
+    out = conv_relu(input)
     print(out)
+    print()
+    assert isinstance(out, DataFlowGraph)
 
+
+def test_dataflow_linking():
+    input = batched_image_tensor(name='input')
+    out = conv_relu(input)
+    out = conv_relu(out)
+    print(out)
+    print()
+    assert isinstance(out, DataFlowGraph)
+
+
+def test_dataflow_block():
+
+    input = batched_image_tensor(name='input')
+    out = block(input)
+    out = block(out)
+    print(out)
+    print()
+
+    assert isinstance(out, DataFlowGraph)
 
 
 if __name__ == '__main__':
     test_dataflow()
+    test_dataflow_linking()
+    test_dataflow_block()
