04f0f5269b40ec71ad9c84c59949996e09f5d27c
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-06-03 13:43:14 +0200

    add tensor

---
+++ hannah/nas/dataflow/tensor.py
@@ -0,0 +1,12 @@
+from hannah.nas.dataflow.tensor_expression import TensorExpression
+
+
+class Tensor(TensorExpression):
+    def __init__(self, *operands, tensor_type=None, name="") -> None:
+        super().__init__(*operands, tensor_type=tensor_type, name=name)
+
+    def __repr__(self) -> str:
+        return "Tensor({})".format(self.name)
+
+    def __str__(self) -> str:
+        return self.__repr__()
