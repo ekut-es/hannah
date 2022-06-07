28d688fab84ea602d35c8287999a7c6677e7b352
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-06-07 16:51:22 +0200

    update test_repeat

--- hannah/nas/test/test_repeat.py
+++ hannah/nas/test/test_repeat.py
@@ -1,6 +1,7 @@
 from hannah.nas.dataflow.dataflow_graph import dataflow, DataFlowGraph
 from hannah.nas.dataflow.op_type import OpType
 from hannah.nas.dataflow.tensor_type import TensorType
+from hannah.nas.expressions.placeholder import UndefinedInt
 from hannah.nas.parameters.parameters import CategoricalParameter, FloatScalarParameter, IntScalarParameter
 from hannah.nas.ops import axis, tensor, batched_image_tensor, float_t
 from hannah.nas.dataflow.repeat import repeat
@@ -13,14 +14,14 @@ def conv_relu(input: TensorType,
               stride=CategoricalParameter([1, 2])):
 
     weight = tensor((axis('o', size=output_channel),
-                     axis('i', size=input.output_tensor().axis['c'].size),
+                     axis('i', size=UndefinedInt()),
                      axis('kh', size=kernel_size),
                      axis('kw', size=kernel_size)),
                     dtype=float_t(),
                     name='weight')
 
-    op = OpType('conv2d', input, weight, stride=stride)
-    relu = OpType('relu', op)
+    op = OpType(input, weight, stride=stride, name='conv2d')
+    relu = OpType(op, name='relu')
     return relu
 
 
@@ -31,19 +32,24 @@ def block(input: TensorType,
           kernel_size=CategoricalParameter([1, 3, 5]),
           stride=CategoricalParameter([1, 2])):
 
-    out = conv_relu(input, output_channel*expansion, kernel_size, stride)
-    out = conv_relu(out, output_channel, 1, 1)
+    out = conv_relu(input,
+                    output_channel=output_channel*expansion,
+                    kernel_size=kernel_size,
+                    stride=stride)
+    out = conv_relu(out,
+                    output_channel=output_channel,
+                    kernel_size=1,
+                    stride=1)
     return out
 
 
 def test_repeat():
     input = batched_image_tensor(name='input')
-    out = repeat(block, num_repeats=4)(input)
-    out = block(out)
-    print(out)
+    graph = repeat(block, num_repeats=IntScalarParameter(min=1, max=5))(input)
+    graph = block(graph)
     print()
 
-    assert isinstance(out, DataFlowGraph)
+    assert isinstance(graph, DataFlowGraph)
 
 
 if __name__ == '__main__':
