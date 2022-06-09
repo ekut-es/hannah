6ae31a2e966e0ac5f023a5d53ba4377c62254d1d
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2022-06-09 18:20:48 +0200

    Document experiment configuration

--- test/test_configs.py
+++ test/test_configs.py
@@ -9,10 +9,21 @@ import hydra
 topdir = pathlib.Path(__file__).parent.absolute() / ".."
 config_dir = topdir / "hannah" / "conf"
 
+project_config_dir = topdir / "configs"
+
 
 def test_parse_configs():
     """This simply tests that all configs are parseable by hydra"""
     for config in config_dir.glob("*.yaml"):
+        with hydra.initialize_config_module(
+            version_base="1.2", config_module="hannah.conf", job_name="test_config"
+        ):
+            cfg = hydra.compose(config_name=config.stem)
+            print("")
+            print("config:", config.stem)
+            print(OmegaConf.to_yaml(cfg))
+
+    for config in project_config_dir.glob("*/*.yaml"):
         with hydra.initialize_config_module(
             version_base="1.2", config_module="hannah.conf", job_name="test_config"
         ):
