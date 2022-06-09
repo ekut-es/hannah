3f475697050ae8f386f8df68a23b3fb51606052a
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2022-06-09 12:25:48 +0200

    Fix singularity image

--- ml_cloud/build_image.sh
+++ ml_cloud/build_image.sh
@@ -7,5 +7,5 @@ elif [[ $EUID -ne 0 ]]; then
    fakeroot="--fakeroot"
 fi
 
-poetry export --without-hashes | grep -v hannah-optimizer > requirements.txt
+poetry export -E vision --without-hashes | grep -v hannah-optimizer > requirements.txt
 singularity build  $fakeroot  ml_cloud.sif  ml_cloud.recipe
