0e92ff16682b1d12114568c575103d483e191ddc
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-06-17 15:52:19 +0200

    add shape func

--- hannah/nas/dataflow/ops/conv2d.py
+++ hannah/nas/dataflow/ops/conv2d.py
@@ -1,24 +1,45 @@
+from hannah.nas.core.expression import Expression
+from hannah.nas.dataflow.axis_type import AxisType
 from hannah.nas.dataflow.data_type import IntType
 from hannah.nas.dataflow.dataflow_graph import dataflow
 from hannah.nas.dataflow.op_type import OpType
 from hannah.nas.dataflow.tensor_expression import TensorExpression
+from hannah.nas.dataflow.tensor_type import TensorType
 from hannah.nas.expressions.placeholder import DefaultInt, UndefinedInt
-from hannah.nas.ops import axis, batched_image_tensor, tensor
-from hannah.nas.dataflow.registry import add_op, add_shape_func, add_conversion, op
+from hannah.nas.parameters.parameters import CategoricalParameter, IntScalarParameter
+from hannah.nas.ops import axis, batched_image_tensor, tensor, weight_tensor
+from hannah.nas.dataflow.registry import add_op, add_shape_func, add_conversion, op, output_tensor
+from hannah.nas.expressions.arithmetic import Floor
 
 
 @add_op
 class Conv2d:
     input: TensorExpression
     weight: TensorExpression
-    size: int
-    dilation: int = 1
-    stride : int = 1
+    # FIXME: DefaultInt or int??
+    kernel_size: Expression
+    dilation: Expression = DefaultInt(1)
+    stride : Expression = DefaultInt(1)
+    padding: Expression = DefaultInt(0)
 
 
-# @add_shape_func("Conv2D")
-# def conv2d_shape(op: OpType):
-#     pass
+@add_shape_func("Conv2d")
+def conv2d_shape(op: OpType):
+    # FIXME: Proprietary floor function?
+
+    def _calc_output_dim(out_dim_name, input_dim, padding, dilation, kernel, stride) -> AxisType:
+        input_size = input_dim.size
+        kernel_size = kernel.size
+        ax = AxisType(name=out_dim_name, size=Floor(((input_size + DefaultInt(2) * padding - dilation * (kernel_size - 1) - 1) / stride) + 1))
+        return ax
+
+    batch = op.input['n']
+    out_channel = op.weight['o'].new('c')  # FIXME: Get channels from weight or conv2d op. Same with kernel_size?
+    output_height = _calc_output_dim('h', op.input['h'], op.padding, op.dilation, op.weight['kh'], op.stride)
+    output_width = _calc_output_dim('w', op.input['w'], op.padding, op.dilation, op.weight['kw'], op.stride)
+
+    # FIXME: Just take inputs dtype?
+    return TensorType((batch, out_channel, output_height, output_width), dtype=op.input.tensor_type.dtype)
 
 
 # @add_conversion("Conv2D", target="torch")
@@ -27,26 +48,28 @@ class Conv2d:
 
 
 @dataflow
-def conv2d(input):
-    kernel_size = UndefinedInt()
-    stride = DefaultInt(1)
-    dilation = DefaultInt(1)
+def conv2d(input, channel, kernel_size=DefaultInt(1), stride=DefaultInt(1), dilation=DefaultInt(1), padding=DefaultInt(0)):
     weight = tensor(
         (
-            axis("o", UndefinedInt()),
-            axis("i", UndefinedInt()),
+            axis("o", channel),
+            axis("i", input['c']),
             axis("kh", kernel_size),
             axis("kw", kernel_size),
         ),
         dtype=IntType(),
+        name="weight"
     )
-    return op("Conv2D", input, weight, size=kernel_size, dilation=dilation, stride=stride)
+    return op("Conv2d", input, weight, kernel_size=kernel_size, dilation=dilation, stride=stride, padding=padding)
 
 
 if __name__ == '__main__':
-    t1 = batched_image_tensor(name="input")
-    t2 = batched_image_tensor(name="weight")
-    # t3 = batched_image_tensor(name="additional_operand")
+    inp = batched_image_tensor(name="input")
 
-    conv = op("Conv2d", t1, t2, size=3, dilation=1)
+    conv = conv2d(inp, channel=UndefinedInt(), kernel_size=CategoricalParameter([1, 3, 5]))
+
+    conv['conv2d.0.Conv2d.0'].kernel_size.set_current(3)
+
+    returned_tensor = output_tensor(conv.output)
+    for name, ax in returned_tensor.axis.items():
+        print("{}: {}".format(name, ax.size.evaluate()))
     print()
