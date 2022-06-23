a04e784e85204927cf6131fa601b43ff96db07bd
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-06-23 15:46:09 +0200

    add chained_convs

--- hannah/nas/dataflow/ops/conv2d.py
+++ hannah/nas/dataflow/ops/conv2d.py
@@ -8,7 +8,8 @@ from hannah.nas.dataflow.tensor_type import TensorType
 from hannah.nas.expressions.placeholder import DefaultInt, UndefinedInt
 from hannah.nas.parameters.parameters import CategoricalParameter, IntScalarParameter
 from hannah.nas.ops import axis, batched_image_tensor, tensor, weight_tensor
-from hannah.nas.dataflow.registry import add_op, add_shape_func, add_conversion, op, output_tensor
+from hannah.nas.dataflow.register_ops import add_op, add_shape_func, add_conversion
+from hannah.nas.dataflow.registry import op
 from hannah.nas.expressions.arithmetic import Floor
 
 
@@ -32,14 +33,17 @@ def conv2d_shape(op: OpType):
         kernel_size = kernel.size
         ax = AxisType(name=out_dim_name, size=Floor(((input_size + DefaultInt(2) * padding - dilation * (kernel_size - 1) - 1) / stride) + 1))
         return ax
+    input = op.input.output_tensor()
+    weight = op.weight.output_tensor()
 
-    batch = op.input['n']
+    batch = input['n']
     out_channel = op.weight['o'].new('c')  # FIXME: Get channels from weight or conv2d op. Same with kernel_size?
-    output_height = _calc_output_dim('h', op.input['h'], op.padding, op.dilation, op.weight['kh'], op.stride)
-    output_width = _calc_output_dim('w', op.input['w'], op.padding, op.dilation, op.weight['kw'], op.stride)
+    output_height = _calc_output_dim('h', input['h'], op.padding, op.dilation, weight['kh'], op.stride)
+    output_width = _calc_output_dim('w', input['w'], op.padding, op.dilation, weight['kw'], op.stride)
 
     # FIXME: Just take inputs dtype?
-    return TensorType((batch, out_channel, output_height, output_width), dtype=op.input.tensor_type.dtype)
+    return TensorType((batch, out_channel, output_height, output_width), dtype=input.tensor_type.dtype)
+
 
 
 # @add_conversion("Conv2D", target="torch")
@@ -49,27 +53,38 @@ def conv2d_shape(op: OpType):
 
 @dataflow
 def conv2d(input, channel, kernel_size=DefaultInt(1), stride=DefaultInt(1), dilation=DefaultInt(1), padding=DefaultInt(0)):
-    weight = tensor(
-        (
-            axis("o", channel),
-            axis("i", input['c']),
-            axis("kh", kernel_size),
-            axis("kw", kernel_size),
-        ),
-        dtype=IntType(),
-        name="weight"
-    )
+    weight = weight_tensor(shape=(channel, input['c'], kernel_size, kernel_size), name='weight')
     return op("Conv2d", input, weight, kernel_size=kernel_size, dilation=dilation, stride=stride, padding=padding)
+
+
+@dataflow
+def chained_convs(input, channel, kernel_size=DefaultInt(1), stride=DefaultInt(1), dilation=DefaultInt(1), padding=DefaultInt(0)):
+    weight1 = weight_tensor(shape=(channel, input['c'], kernel_size, kernel_size), name='weight')
+    conv1 = op("Conv2d", input, weight1, kernel_size=kernel_size, dilation=dilation, stride=stride, padding=padding)
+
+    weight2 = weight_tensor(shape=(channel, input['c'], kernel_size, kernel_size), name='weight')
+    conv2 = op("Conv2d", conv1, weight2, kernel_size=kernel_size, dilation=dilation, stride=stride, padding=padding)
+
+    return conv2
 
 
 if __name__ == '__main__':
     inp = batched_image_tensor(name="input")
 
-    conv = conv2d(inp, channel=UndefinedInt(), kernel_size=CategoricalParameter([1, 3, 5]))
+    conv = conv2d(inp, channel=IntScalarParameter(4, 64), kernel_size=CategoricalParameter([1, 3, 5]))
 
     conv['conv2d.0.Conv2d.0'].kernel_size.set_current(3)
 
-    returned_tensor = output_tensor(conv.output)
-    for name, ax in returned_tensor.axis.items():
+    returned_tensor = conv.output.output_tensor()
+    for name, ax in returned_tensor.tensor_type.axis.items():
         print("{}: {}".format(name, ax.size.evaluate()))
+
+    ks = CategoricalParameter([1, 3, 5])
+    ks.set_current(5)
+    convs = chained_convs(inp, channel=IntScalarParameter(4, 64), kernel_size=ks)
+    returned_tensor = convs.output.output_tensor()
+
+    for name, ax in returned_tensor.tensor_type.axis.items():
+        print("{}: {}".format(name, ax.size.evaluate()))
+
     print()
