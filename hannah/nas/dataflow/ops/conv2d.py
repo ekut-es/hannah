d9f1a94ebc9cfc871590697c4606210de4dfd2cd
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-06-23 16:02:16 +0200

    move testing code to dedicated test_conv2d.py

--- hannah/nas/dataflow/ops/conv2d.py
+++ hannah/nas/dataflow/ops/conv2d.py
@@ -1,15 +1,10 @@
 from hannah.nas.core.expression import Expression
 from hannah.nas.dataflow.axis_type import AxisType
-from hannah.nas.dataflow.data_type import IntType
-from hannah.nas.dataflow.dataflow_graph import dataflow
 from hannah.nas.dataflow.op_type import OpType
 from hannah.nas.dataflow.tensor_expression import TensorExpression
 from hannah.nas.dataflow.tensor_type import TensorType
-from hannah.nas.expressions.placeholder import DefaultInt, UndefinedInt
-from hannah.nas.parameters.parameters import CategoricalParameter, IntScalarParameter
-from hannah.nas.ops import axis, batched_image_tensor, tensor, weight_tensor
+from hannah.nas.expressions.placeholder import DefaultInt
 from hannah.nas.dataflow.register_ops import add_op, add_shape_func, add_conversion
-from hannah.nas.dataflow.registry import op
 from hannah.nas.expressions.arithmetic import Floor
 
 
@@ -26,8 +21,6 @@ class Conv2d:
 
 @add_shape_func("Conv2d")
 def conv2d_shape(op: OpType):
-    # FIXME: Proprietary floor function?
-
     def _calc_output_dim(out_dim_name, input_dim, padding, dilation, kernel, stride) -> AxisType:
         input_size = input_dim.size
         kernel_size = kernel.size
@@ -45,46 +38,6 @@ def conv2d_shape(op: OpType):
     return TensorType((batch, out_channel, output_height, output_width), dtype=input.tensor_type.dtype)
 
 
-
-# @add_conversion("Conv2D", target="torch")
-# def conv2d_torch(op: OpType):
-#     pass
-
-
-@dataflow
-def conv2d(input, channel, kernel_size=DefaultInt(1), stride=DefaultInt(1), dilation=DefaultInt(1), padding=DefaultInt(0)):
-    weight = weight_tensor(shape=(channel, input['c'], kernel_size, kernel_size), name='weight')
-    return op("Conv2d", input, weight, kernel_size=kernel_size, dilation=dilation, stride=stride, padding=padding)
-
-
-@dataflow
-def chained_convs(input, channel, kernel_size=DefaultInt(1), stride=DefaultInt(1), dilation=DefaultInt(1), padding=DefaultInt(0)):
-    weight1 = weight_tensor(shape=(channel, input['c'], kernel_size, kernel_size), name='weight')
-    conv1 = op("Conv2d", input, weight1, kernel_size=kernel_size, dilation=dilation, stride=stride, padding=padding)
-
-    weight2 = weight_tensor(shape=(channel, input['c'], kernel_size, kernel_size), name='weight')
-    conv2 = op("Conv2d", conv1, weight2, kernel_size=kernel_size, dilation=dilation, stride=stride, padding=padding)
-
-    return conv2
-
-
-if __name__ == '__main__':
-    inp = batched_image_tensor(name="input")
-
-    conv = conv2d(inp, channel=IntScalarParameter(4, 64), kernel_size=CategoricalParameter([1, 3, 5]))
-
-    conv['conv2d.0.Conv2d.0'].kernel_size.set_current(3)
-
-    returned_tensor = conv.output.output_tensor()
-    for name, ax in returned_tensor.tensor_type.axis.items():
-        print("{}: {}".format(name, ax.size.evaluate()))
-
-    ks = CategoricalParameter([1, 3, 5])
-    ks.set_current(5)
-    convs = chained_convs(inp, channel=IntScalarParameter(4, 64), kernel_size=ks)
-    returned_tensor = convs.output.output_tensor()
-
-    for name, ax in returned_tensor.tensor_type.axis.items():
-        print("{}: {}".format(name, ax.size.evaluate()))
-
-    print()
+@add_conversion("Conv2D", target="torch")
+def conv2d_torch(op: OpType):
+    pass
---
+++ hannah/nas/test/test_conv2d.py
@@ -0,0 +1,53 @@
+from hannah.nas.dataflow.dataflow_graph import dataflow
+from hannah.nas.dataflow.ops import conv2d  # Import to load in registry
+from hannah.nas.dataflow.registry import op
+from hannah.nas.expressions.placeholder import DefaultInt
+from hannah.nas.ops import batched_image_tensor, weight_tensor
+from hannah.nas.parameters.parameters import CategoricalParameter, IntScalarParameter
+
+
+@dataflow
+def conv2d(input, channel, kernel_size=DefaultInt(1), stride=DefaultInt(1), dilation=DefaultInt(1), padding=DefaultInt(0)):
+    weight = weight_tensor(shape=(channel, input['c'], kernel_size, kernel_size), name='weight')
+    return op("Conv2d", input, weight, kernel_size=kernel_size, dilation=dilation, stride=stride, padding=padding)
+
+
+@dataflow
+def chained_convs(input, channel, kernel_size=DefaultInt(1), stride=DefaultInt(1), dilation=DefaultInt(1), padding=DefaultInt(0)):
+    weight1 = weight_tensor(shape=(channel, input['c'], kernel_size, kernel_size), name='weight')
+    conv1 = op("Conv2d", input, weight1, kernel_size=kernel_size, dilation=dilation, stride=stride, padding=padding)
+
+    weight2 = weight_tensor(shape=(channel, input['c'], kernel_size, kernel_size), name='weight')
+    conv2 = op("Conv2d", conv1, weight2, kernel_size=kernel_size, dilation=dilation, stride=stride, padding=padding)
+
+    return conv2
+
+
+def test_conv2d():
+    inp = batched_image_tensor(name="input")
+
+    conv = conv2d(inp, channel=IntScalarParameter(4, 64), kernel_size=CategoricalParameter([1, 3, 5]))
+
+    conv['conv2d.0.Conv2d.0'].kernel_size.set_current(3)
+
+    returned_tensor = conv.output.output_tensor()
+    for name, ax in returned_tensor.tensor_type.axis.items():
+        print("{}: {}".format(name, ax.size.evaluate()))
+
+
+def test_chained_conv2d():
+    inp = batched_image_tensor(name="input")
+
+    ks = CategoricalParameter([1, 3, 5])
+    ks.set_current(5)
+    convs = chained_convs(inp, channel=IntScalarParameter(4, 64), kernel_size=ks)
+    returned_tensor = convs.output.output_tensor()
+
+    for name, ax in returned_tensor.tensor_type.axis.items():
+        print("{}: {}".format(name, ax.size.evaluate()))
+
+
+if __name__ == '__main__':
+    test_conv2d()
+    test_chained_conv2d()
+    print()
