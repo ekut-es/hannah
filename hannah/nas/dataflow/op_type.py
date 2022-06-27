9d6c697f477db52442f8bdb644ac7b9f8a00c723
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-06-27 11:28:20 +0200

    add _parameter collection

--- hannah/nas/dataflow/op_type.py
+++ hannah/nas/dataflow/op_type.py
@@ -2,14 +2,19 @@ from hannah.nas.dataflow.dataflow_utils import find_first_op_in_dfg
 from hannah.nas.dataflow.tensor import Tensor
 from hannah.nas.dataflow.tensor_expression import TensorExpression
 from hannah.nas.dataflow.registry import shape
+from hannah.nas.parameters.parameters import Parameter
 
 
 class OpType(TensorExpression):
     def __init__(self, *operands, tensor_type=None, name="", **attributes):
         super().__init__(*operands, tensor_type=tensor_type, name=name)
 
+        self._parameters = {}
+
         for name, attribute in attributes.items():
             setattr(self, name, attribute)
+            if isinstance(attribute, Parameter):
+                self._parameters[name] = attribute
 
         self.attributes = attributes
         self.link_users()
