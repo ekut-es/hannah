b8433cac024c5ce54af471f298b2bcc7662b9718
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-07-25 15:57:59 +0200

    update tests

--- hannah/nas/test/test_dataflow.py
+++ hannah/nas/test/test_dataflow.py
@@ -1,8 +1,12 @@
-from hannah.nas.dataflow.dataflow_graph import dataflow, DataFlowGraph
+from requests import delete
+from hannah.nas.dataflow.dataflow_graph import dataflow, DataFlowGraph, delete_users
 from hannah.nas.dataflow.op_type import OpType
+from hannah.nas.dataflow.registry import op
 from hannah.nas.dataflow.tensor_type import TensorType
+from hannah.nas.expressions.placeholder import DefaultInt
 from hannah.nas.parameters.parameters import CategoricalParameter, FloatScalarParameter, IntScalarParameter
-from hannah.nas.ops import axis, tensor, batched_image_tensor, float_t
+from hannah.nas.ops import axis, tensor, batched_image_tensor, float_t, weight_tensor
+from hannah.nas.dataflow.ops import conv2d, add  # noqa: F401 (Import to load in registry)
 
 
 @dataflow
@@ -10,16 +14,11 @@ def conv_relu(input: TensorType,
               output_channel=IntScalarParameter(4, 64),
               kernel_size=CategoricalParameter([1, 3, 5]),
               stride=CategoricalParameter([1, 2])):
+    input_tensor = input.output_tensor()
+    weight = weight_tensor(shape=(output_channel, input_tensor['c'], kernel_size, kernel_size), name='weight')
 
-    weight = tensor((axis('o', size=output_channel),
-                     axis('i', size=input.output_tensor().axis['c'].size),
-                     axis('kh', size=kernel_size),
-                     axis('kw', size=kernel_size)),
-                    dtype=float_t(),
-                    name='weight')
-
-    op = OpType('conv2d', input, weight, stride=stride)
-    relu = OpType('relu', op)
+    c = op("Conv2d", input, weight, stride=stride)
+    relu = OpType(c, name='Relu')
     return relu
 
 
@@ -30,8 +29,14 @@ def block(input: TensorType,
           kernel_size=CategoricalParameter([1, 3, 5]),
           stride=CategoricalParameter([1, 2])):
 
-    out = conv_relu(input, output_channel*expansion, kernel_size, stride)
-    out = conv_relu(out, output_channel, 1, 1)
+    out = conv_relu(input, output_channel=output_channel*expansion, kernel_size=kernel_size, stride=DefaultInt(1))
+    out = conv_relu(out, output_channel=output_channel, kernel_size=DefaultInt(1), stride=stride)
+    return out
+
+
+@dataflow
+def add(input: TensorType, other: TensorType):
+    out = op('Add', input, other)
     return out
 
 
@@ -39,7 +44,6 @@ def test_dataflow():
     input = batched_image_tensor(name='input')
     out = conv_relu(input)
     print(out)
-    print()
     assert isinstance(out, DataFlowGraph)
 
 
@@ -48,22 +52,29 @@ def test_dataflow_linking():
     out = conv_relu(input)
     out = conv_relu(out)
     print(out)
-    print()
     assert isinstance(out, DataFlowGraph)
 
 
 def test_dataflow_block():
-
     input = batched_image_tensor(name='input')
-    out = block(input)
-    out = block(out)
+    out = block(input, stride=DefaultInt(1))
+    out = block(out, stride=DefaultInt(2))
     print(out)
-    print()
 
     assert isinstance(out, DataFlowGraph)
+
+
+def test_parallel_blocks():
+    input = batched_image_tensor(name='input')
+    graph_0 = block(input)
+    graph_1 = block(input)
+    graph = add(graph_0, graph_1)
+    print(graph)
+
 
 
 if __name__ == '__main__':
     test_dataflow()
     test_dataflow_linking()
     test_dataflow_block()
+    test_parallel_blocks()
