12b88f49cf84c8eb1cc53c2c17d96eb98366619a
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2022-08-22 17:27:59 +0200

    Update hannah-tvm

--- hydra_plugins/hannah_search_path/hannah_searchpath_plugin.py
+++ hydra_plugins/hannah_search_path/hannah_searchpath_plugin.py
@@ -1,3 +1,21 @@
+#
+# Copyright (c) 2022 University of Tübingen.
+#
+# This file is part of hannah.
+# See https://atreus.informatik.uni-tuebingen.de/ties/ai/hannah/hannah for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import logging
 import pathlib
 from typing import Optional
@@ -16,7 +34,7 @@ class HannahSearchPathPlugin(SearchPathPlugin):
         # Add hannah_tvm to search path
         try:
             import hannah_tvm.conf as conf
-            import hannah_tvm.config  # noqa
+            import hannah_tvm.config as config  # noqa
         except ModuleNotFoundError:
             logging.debug(
                 "Could not find hannah_tvm.conf, tvm backend is not available"
