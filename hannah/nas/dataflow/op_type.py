73d1b638953d4efd924e9c54138fc69b52add24c
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-08-24 16:29:56 +0200

    parametrize op_type

--- hannah/nas/dataflow/op_type.py
+++ hannah/nas/dataflow/op_type.py
@@ -1,21 +1,30 @@
+from copy import deepcopy
+from hannah.nas.core.parametrized import is_parametrized
 from hannah.nas.dataflow.dataflow_utils import find_first_op_in_dfg, find_leaf_nodes
 from hannah.nas.dataflow.scoping_utils import get_id_and_update_counters, update_scope
 from hannah.nas.dataflow.tensor import Tensor
 from hannah.nas.dataflow.tensor_expression import TensorExpression
 import hannah.nas.dataflow.registry as reg
 from hannah.nas.parameters.parameters import Parameter
+from hannah.nas.parameters.parametrize import parametrize
 
 
+@parametrize
 class OpType(TensorExpression):
     def __init__(self, *operands, tensor_type=None, name="", **attributes):
         super().__init__(*operands, tensor_type=tensor_type, name=name)
 
-        self._parameters = {}
+        self._PARAMETERS = {}
+        # self._conditions = []
+
+        for i, operand in enumerate(operands):
+            if is_parametrized(operand):
+                self._PARAMETERS[i] = operand
 
         for name, attribute in attributes.items():
             setattr(self, name, attribute)
-            if isinstance(attribute, Parameter):
-                self._parameters[name] = attribute
+            if is_parametrized(attribute):
+                self._PARAMETERS[name] = attribute
 
         self.attributes = attributes
         self.link_users()
@@ -30,6 +39,7 @@ class OpType(TensorExpression):
         current_scope = update_scope(self, current_scope)
         scope_id = get_id_and_update_counters(current_scope, counters)
         self.id = scope_id
+        self.set_param_scopes()
         visited.append(self)
 
         leafs = []
@@ -40,9 +50,37 @@ class OpType(TensorExpression):
             leaf.set_scope(current_scope, counters, visited)
             visited.append(leaf)
 
-    def output_tensor(self):
+    def tensor_type(self):
         tensortype = reg.shape(self.name)(self)
-        return Tensor(tensor_type=tensortype, name=self.id + '.output')
+
+        return tensortype
+
+    # def sample(self):
+    #     for _key, param in self._PARAMETERS.items():
+    #         param.sample()
+
+    # def set_current(self, value):
+    #     self.set_params(**value)
+    #     self.check(None)  # argument "value" not needed currently
+
+    # def check(self, value):
+    #     for con in self._conditions:
+    #         if not con.evaluate():
+    #             raise Exception("Condition not satisfied: {}".format(con))
+
+    # def instantiate(self):
+    #     instance = deepcopy(self)
+    #     instance._parametrized = False
+    #     self.check(None)
+
+    #     for key, param in instance._PARAMETERS.items():
+    #         instantiated_value = param.instantiate()
+    #         instance._PARAMETERS[key] = instantiated_value
+    #         setattr(instance, key, instantiated_value)
+    #     return instance
+
+    # def parameters(self):
+    #     return self._PARAMETERS
 
     def convert(self, target):
         return reg.convert(self.name, target)(self)
