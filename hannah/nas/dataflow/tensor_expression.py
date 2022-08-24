8360009ce41020616b1ae21c74d11fc018a4e613
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-08-24 16:30:57 +0200

    add tensor_type and param_scopes

--- hannah/nas/dataflow/tensor_expression.py
+++ hannah/nas/dataflow/tensor_expression.py
@@ -1,12 +1,21 @@
-
+from hannah.nas.core.expression import Expression
 
 class TensorExpression:
     def __init__(self, *operands, tensor_type=None, name="") -> None:
         self.operands = operands
-        self.tensor_type = tensor_type
+        self._tensor_type = tensor_type
         self.users = []
         self.name = name
         self.id = name
 
     def set_id(self, id):
         self.id = id
+
+    def tensor_type(self):
+        assert self._tensor_type is not None, "Tensor Type has not been set, please run shape inference"
+        return self._tensor_type
+
+    def set_param_scopes(self):
+        for name, param in self._PARAMETERS.items():
+            if isinstance(param, Expression):
+                param.id = self.id + '.' + name
