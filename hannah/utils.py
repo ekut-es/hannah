85abf6c43a524b0fa7f04cd6893c7d94a2a06762
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2022-09-14 10:55:00 +0200

    Fix trainer config

--- hannah/utils.py
+++ hannah/utils.py
@@ -1,3 +1,21 @@
+#
+# Copyright (c) 2022 University of Tübingen.
+#
+# This file is part of hannah.
+# See https://atreus.informatik.uni-tuebingen.de/ties/ai/hannah/hannah for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import importlib
 import logging
 import os
@@ -22,7 +40,6 @@ from pl_bolts.callbacks import ModuleDataMonitor, PrintTableMetricsCallback
 from pytorch_lightning.callbacks import (
     Callback,
     DeviceStatsMonitor,
-    GPUStatsMonitor,
     LearningRateMonitor,
 )
 from pytorch_lightning.loggers import CSVLogger, TensorBoardLogger
@@ -201,10 +218,6 @@ def common_callbacks(config: DictConfig) -> list:
 
     lr_monitor = LearningRateMonitor()
     callbacks.append(lr_monitor)
-
-    if config.get("gpu_stats", None):
-        gpu_stats = GPUStatsMonitor()
-        callbacks.append(gpu_stats)
 
     if config.get("device_stats", None):
         device_stats = DeviceStatsMonitor()
