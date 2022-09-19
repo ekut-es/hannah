271c144ce73ad56cd2c0726a66ce7f192357419d
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2022-09-19 12:21:15 +0200

    Add base model for ovsf dataset

--- hannah/models/factory/factory.py
+++ hannah/models/factory/factory.py
@@ -1,3 +1,21 @@
+#
+# Copyright (c) 2022 University of Tübingen.
+#
+# This file is part of hannah.
+# See https://atreus.informatik.uni-tuebingen.de/ties/ai/hannah/hannah for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 """ A neural network model factory
 
 It allows us to construct quantized and unquantized versions of the same network,
@@ -155,7 +173,6 @@ class NetworkFactory:
         act: Union[ActConfig, bool] = False,
         bias: bool = False,
     ) -> None:
-
         in_channels = input_shape[1]
 
         if isinstance(kernel_size, int):
