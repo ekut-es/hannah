ef72937ede6a07cb2b779a890cce2af7ad03a70e
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-09-30 13:36:46 +0200

    add scoping for parameters

--- hannah/nas/dataflow/tensor_expression.py
+++ hannah/nas/dataflow/tensor_expression.py
@@ -1,5 +1,3 @@
-from hannah.nas.core.expression import Expression
-
 class TensorExpression:
     def __init__(self, *operands, tensor_type=None, name="") -> None:
         self.operands = operands
@@ -14,8 +12,3 @@ class TensorExpression:
     def tensor_type(self):
         assert self._tensor_type is not None, "Tensor Type has not been set, please run shape inference"
         return self._tensor_type
-
-    def set_param_scopes(self):
-        for name, param in self._PARAMETERS.items():
-            if isinstance(param, Expression):
-                param.id = self.id + '.' + name
