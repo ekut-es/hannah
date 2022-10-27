d48e5ed5ecd485d99f4c18a6eb003637135c3050
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-10-27 11:31:37 +0200

    add next functions and outsource collect_scopes

--- hannah/nas/dataflow/tensor_expression.py
+++ hannah/nas/dataflow/tensor_expression.py
@@ -5,6 +5,7 @@ class TensorExpression:
         self.users = []
         self.name = name
         self.id = name
+        self._scopes = {}
 
     def set_id(self, id):
         self.id = id
@@ -12,3 +13,24 @@ class TensorExpression:
     def tensor_type(self):
         assert self._tensor_type is not None, "Tensor Type has not been set, please run shape inference"
         return self._tensor_type
+
+    def next_backwards(self):
+        return []
+
+    def next_forwards(self):
+        return []
+
+    def collect_scopes(self):
+        queue = [self]
+        visited = [self]
+
+        while queue:
+            current = queue.pop(-1)
+            self._scopes[current.id] = current
+
+            for next_node in current.next_backwards():
+                if next_node not in visited:
+                    queue.append(next_node)
+                    visited.append(next_node)
+
+        self._scopes = dict(sorted(self._scopes.items()))
