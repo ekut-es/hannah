512b53d9c7cd78e27796a0dbf1a471c2383f928d
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2022-11-10 10:02:26 +0100

    Enable confusion matrix in image classifier again

--- hannah/modules/image_classifier.py
+++ hannah/modules/image_classifier.py
@@ -215,10 +215,10 @@ class ImageClassifierModule(ClassifierModule):
                 self._log_batch_images("decoded", batch_idx, decoded)
 
         self.log(f"{step_name}_loss", loss)
-        return loss, prediction_result, batch
+        return loss, prediction_result, batch, preds
 
     def training_step(self, batch, batch_idx):
-        loss, _, _ = self.common_step("train", batch, batch_idx)
+        loss, _, _, _ = self.common_step("train", batch, batch_idx)
 
         return loss
 
@@ -226,12 +226,12 @@ class ImageClassifierModule(ClassifierModule):
         self.common_step("val", batch, batch_idx)
 
     def test_step(self, batch, batch_idx):
-        _, step_results, batch = self.common_step("test", batch, batch_idx)
+        _, step_results, batch, preds = self.common_step("test", batch, batch_idx)
 
         y = batch.get("labels", None)
-        # if y is not None and preds is not None:
-        #    with set_deterministic(False):
-        #        self.test_confusion(preds, y)
+        if y is not None and preds is not None:
+            with set_deterministic(False):
+                self.test_confusion(preds, y)
 
     def on_train_epoch_end(self):
         self.eval()
