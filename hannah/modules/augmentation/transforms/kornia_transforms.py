f64cc0d3c8e43d14519fae3cb51caa8904daccaa
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2022-11-14 17:51:36 +0100

    Fix missing kornia

--- hannah/modules/augmentation/transforms/kornia_transforms.py
+++ hannah/modules/augmentation/transforms/kornia_transforms.py
@@ -16,35 +16,45 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import kornia.augmentation as A
+
+import logging
+
+try:
+    import kornia.augmentation as A
+except ModuleNotFoundError:
+    logging.warning(
+        "Could not import kornia augmentations if needed install with -E vision"
+    )
+    A = None
 
 from .registry import registry
 
 # Intensity Transformations
-registry.register(A.RandomMotionBlur)
-registry.register(A.ColorJiggle)
-registry.register(A.RandomBoxBlur)
-registry.register(A.RandomChannelShuffle)
-registry.register(A.RandomEqualize)
-registry.register(A.RandomGrayscale)
-registry.register(A.RandomGaussianBlur)
-registry.register(A.RandomGaussianNoise)
-registry.register(A.RandomMotionBlur)
-registry.register(A.RandomPosterize)
-registry.register(A.RandomSharpness)
-registry.register(A.RandomSolarize)
+if A:
+    registry.register(A.RandomMotionBlur)
+    registry.register(A.ColorJiggle)
+    registry.register(A.RandomBoxBlur)
+    registry.register(A.RandomChannelShuffle)
+    registry.register(A.RandomEqualize)
+    registry.register(A.RandomGrayscale)
+    registry.register(A.RandomGaussianBlur)
+    registry.register(A.RandomGaussianNoise)
+    registry.register(A.RandomMotionBlur)
+    registry.register(A.RandomPosterize)
+    registry.register(A.RandomSharpness)
+    registry.register(A.RandomSolarize)
 
-# Color Transformations
-registry.register(A.CenterCrop)
-registry.register(A.RandomAffine)
-registry.register(A.RandomCrop)
-registry.register(A.RandomErasing)
-registry.register(A.RandomElasticTransform)
-registry.register(A.RandomFisheye)
-registry.register(A.RandomHorizontalFlip)
-registry.register(A.RandomInvert)
-registry.register(A.RandomPerspective)
-registry.register(A.RandomResizedCrop)
-registry.register(A.RandomRotation)
-registry.register(A.RandomVerticalFlip)
-registry.register(A.RandomThinPlateSpline)
+    # Color Transformations
+    registry.register(A.CenterCrop)
+    registry.register(A.RandomAffine)
+    registry.register(A.RandomCrop)
+    registry.register(A.RandomErasing)
+    registry.register(A.RandomElasticTransform)
+    registry.register(A.RandomFisheye)
+    registry.register(A.RandomHorizontalFlip)
+    registry.register(A.RandomInvert)
+    registry.register(A.RandomPerspective)
+    registry.register(A.RandomResizedCrop)
+    registry.register(A.RandomRotation)
+    registry.register(A.RandomVerticalFlip)
+    registry.register(A.RandomThinPlateSpline)
