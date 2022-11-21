3ec3d5e860618141d465df07530bb2a3dfa225e0
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-11-21 16:16:11 +0100

    add shape property

--- hannah/nas/parameters/lazy.py
+++ hannah/nas/parameters/lazy.py
@@ -1,4 +1,3 @@
-from hannah.nas.parameters.parameters import Parameter
 from hannah.nas.expressions.shapes import identity_shape
 from copy import deepcopy
 
@@ -10,8 +9,10 @@ class Lazy:
         self.kwargs = {}
         self.shape_func = shape_func
 
-    def __call__(self, id: str, *args, **kwargs):
+    def __call__(self, id: str, *args, inputs: list = [], **kwargs):
         self._id = id
+        self.inputs = inputs
+
         self.args = args
         self.kwargs = kwargs
 
@@ -24,11 +25,20 @@ class Lazy:
         else:
             raise AttributeError("Lazy objects have to be initialized to have an id.")
 
-    def shape(self, input_shape):
-        return self.shape_func(input_shape, self.kwargs)
+    @property
+    def shape(self):
+        if hasattr(self, '_output_shape'):
+            return self._output_shape
+        else:
+            self._output_shape = self.shape_func(*[inp.shape if hasattr(inp, 'shape') else inp for inp in self.inputs], kwargs=self.kwargs)
+            return self._output_shape
 
     def __repr__(self) -> str:
-        return "Lazy({})".format(self.klass.__name__)
+        if hasattr(self, '_id'):
+            name = self._id
+        else:
+            name = self.klass.__name__
+        return "Lazy({})".format(name)
 
-    def sample(self):
+    def instantiate(self):
         return self.klass(*[arg.evaluate() if hasattr(arg, 'evaluate') else arg for arg in self.args], **{name: arg.evaluate() if hasattr(arg, 'evaluate') else arg for name, arg in self.kwargs.items()})
