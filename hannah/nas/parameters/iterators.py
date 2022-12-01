2352935a0131b2637b8c1ebf75c99b833ba760c7
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2022-12-01 10:15:10 +0100

    count from 0 to n - 1 instead of 1 to n

--- hannah/nas/parameters/iterators.py
+++ hannah/nas/parameters/iterators.py
@@ -2,13 +2,13 @@ class RangeIterator:
     def __init__(self, parameter, instance=False) -> None:
         self.parameter = parameter
         self.instance = instance
-        self.counter = parameter.min - 1
+        self.counter = 0 - 1
 
     def __iter__(self):
         return self
 
     def __next__(self):
         self.counter += 1
-        if self.counter <= (self.parameter.current_value if self.instance else self.parameter.max):
+        if self.counter < (self.parameter.current_value if self.instance else self.parameter.max):
             return self.counter
         raise StopIteration
