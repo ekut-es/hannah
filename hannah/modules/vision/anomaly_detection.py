32fc7e310d79d969e2829df596d7bd97f62ef12c
wernerju <julia-helga.werner@student.uni-tuebingen.de>
2022-12-15 16:17:38 +0100

    Moved configuration of anomaly detection to conf of anomaly detection module, error fixes

--- hannah/modules/vision/anomaly_detection.py
+++ hannah/modules/vision/anomaly_detection.py
@@ -71,10 +71,7 @@ class AnomalyDetectionModule(VisionBaseModule):
         loss = torch.tensor([0.0], device=self.device)
         preds = None
 
-        if (
-            self.hparams.anomaly_detection_loss.loss == "classifier"
-            or step_name == "test"
-        ):
+        if self.hparams.train_val_loss == "classifier" or step_name == "test":
             loss = ss_loss.forward(logits=prediction_result.logits, labels=labels)
             self.log(f"{step_name}_classifier_loss", loss)
             preds = torch.argmax(prediction_result.logits, dim=1)
@@ -82,7 +79,7 @@ class AnomalyDetectionModule(VisionBaseModule):
             self.log_dict(self.metrics[f"{step_name}_metrics"])
 
         elif (
-            self.hparams.anomaly_detection_loss.loss == "decoder"
+            self.hparams.train_val_loss == "decoder"
             and prediction_result.decoded is not None
         ):
             decoded = prediction_result.decoded
@@ -123,13 +120,13 @@ class AnomalyDetectionModule(VisionBaseModule):
                 current_loss = torch.tensor([0.0], device=self.device)
                 augmented_data, x = self.augment(x, labels, boxes, batch_idx)
                 prediction_result = self.forward(augmented_data)
-                if not self.hparams.anomaly_detection_loss:
+                if not self.hparams.train_val_loss:
                     raise ValueError(
                         "Please specify the desired loss function for training with the anomaly detection module"
                     )
 
                 elif (
-                    self.hparams.anomaly_detection_loss.loss == "decoder"
+                    self.hparams.train_val_loss == "decoder"
                     and prediction_result.decoded is not None
                 ):
                     decoded = prediction_result.decoded
@@ -143,7 +140,7 @@ class AnomalyDetectionModule(VisionBaseModule):
                         self._log_batch_images("decoded", batch_idx, decoded)
 
                 elif (
-                    self.hparams.anomaly_detection_loss.loss == "classifier"
+                    self.hparams.train_val_loss == "classifier"
                     and labels is not None
                     and prediction_result.logits.numel() > 0
                 ):
@@ -157,7 +154,7 @@ class AnomalyDetectionModule(VisionBaseModule):
                     self.log_dict(self.metrics["train_metrics"])
 
                 self.log(
-                    "train_" + f"{self.hparams.anomaly_detection_loss.loss}_loss",
+                    "train_" + f"{self.hparams.train_val_loss}_loss",
                     current_loss,
                 )
                 loss += current_loss
@@ -171,7 +168,7 @@ class AnomalyDetectionModule(VisionBaseModule):
             "val", batch, batch_idx
         )
         labels = batch.get("labels", None)
-        if self.hparams.anomaly_detection_loss.loss == "decoder":
+        if self.hparams.train_val_loss == "decoder":
             anomaly_score, largest_train_error = self.compute_anomaly_score()
             if anomaly_score:
                 if (loss.cpu().numpy() / largest_train_error) > anomaly_score:
@@ -200,7 +197,7 @@ class AnomalyDetectionModule(VisionBaseModule):
             self.labels = torch.cat((self.labels.to(y.device), y), 0)
 
     def on_test_end(self):
-        wd_dir = os.getcwd()
+        """wd_dir = os.getcwd()
         score, largest_train_error = self.compute_anomaly_score()
         train_errors = self.normalized_train_errors
         plt.hist(train_errors.detach().cpu().numpy(), bins=100)
@@ -208,35 +205,35 @@ class AnomalyDetectionModule(VisionBaseModule):
         plt.title("Normalized train reconstruction errors")
         plt.savefig(wd_dir + "/normalized_train_errors.png")
 
-        """test = (
+        test = (
             torch.tensor(self.test_losses, device=self.device)
             / torch.max(torch.stack(self.train_losses), dim=0).values
         )
         plt.hist(test.detach().cpu().numpy(), bins=100)
         plt.title("Normalized test reconstruction errors")
-        plt.savefig(wd_dir + "/normalized_test_errors.png")"""
-
-        self._plot_confusion_matrix()
-        self._AUROC(self.predictions, self.labels)
+        plt.savefig(wd_dir + "/normalized_test_errors.png")
         print("Anomaly score", score)
         print(
             "Largest train error",
             torch.max(torch.stack(self.train_losses), dim=0).values,
-        )
+        )"""
+
+        self._plot_confusion_matrix()
+        self._AUROC(self.predictions, self.labels)
 
     def on_train_epoch_end(self):
         self.train_losses = self.train_losses[-1000:]
         super().on_train_epoch_end()
 
     def on_train_end(self):
-        if self.hparams.anomaly_detection_loss.loss == "decoder":
+        if self.hparams.train_val_loss == "decoder":
             optimizer = torch.optim.AdamW(self.model.classifier.parameters(), lr=0.001)
-            for epoch in range(1):
+            for epoch in range(10):
                 print("Training epoch of linear classifier: ", epoch)
                 counter = 0
                 for batch in self.train_dataloader():
                     counter += 1
-                    if counter % 5000 == 0:
+                    if counter % 5 == 0:
                         labeled_batch = batch["labeled"]
                         x = labeled_batch["data"]
                         labels = labeled_batch.get("labels", None).to(
