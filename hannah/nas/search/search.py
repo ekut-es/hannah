3cb272c8a09407e0461dbae9f1e46886f771e026
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2023-01-16 15:59:36 +0100

    add parallel search to random nas

--- hannah/nas/search/search.py
+++ hannah/nas/search/search.py
@@ -19,19 +19,13 @@
 
 import logging
 from joblib import Parallel, delayed
-import numpy as np
 from omegaconf import OmegaConf
 import torch
 import os
 
 from abc import ABC, abstractmethod
-from copy import deepcopy
-from pathlib import Path
 from hydra.utils import instantiate, get_class
-import yaml
 from hannah.callbacks.optimization import HydraOptCallback
-from hannah.nas.search.model_trainer.simple_model_trainer import SimpleModelTrainer
-from hannah.nas.search.sampler.aging_evolution import AgingEvolution
 from hannah.nas.search.utils import WorklistItem, save_config_to_file
 from hannah.utils.utils import common_callbacks
 from hannah.nas.graph_conversion import model_to_graph
@@ -41,11 +35,13 @@ msglogger = logging.getLogger(__name__)
 class NASBase(ABC):
     def __init__(self,
                  budget=2000,
+                 n_jobs=1,
                  sampler=None,
                  model_trainer=None,
                  predictor=None,
                  parent_config=None) -> None:
         self.budget = budget
+        self.n_jobs = n_jobs
         self.config = parent_config
         self.callbacks = []
         self.sampler = sampler
@@ -59,15 +55,15 @@ class NASBase(ABC):
 
     @abstractmethod
     def before_search(self):
-        pass
+        ...
 
     @abstractmethod
     def search(self):
-        pass
+        ...
 
     @abstractmethod
     def after_search(self):
-        pass
+        ...
 
     def add_model_trainer(self, trainer):
         self.model_trainer = trainer
@@ -84,26 +80,51 @@ class DirectNAS(NASBase):
         super().__init__(*args, budget=budget, **kwargs)
 
     def before_search(self):
-        # setup logging
         self.initialize_dataset()
         self.search_space = self.build_search_space()
-        parameters = self.search_space.parametrization(flatten=True)
-        self.sampler = instantiate(self.config.nas.sampler, parameters=parameters)
+        parametrization = self.search_space.parametrization(flatten=True)
+        self.sampler = instantiate(self.config.nas.sampler, parametrization=parametrization)
         self.model_trainer = instantiate(self.config.nas.model_trainer)
 
     def search(self):
-        for i in range(self.budget):
-            self.global_num = i
-            self.setup_model_logging()
-            parameters = self.sample()
-            model = self.model_trainer.build_model(self.search_space, self.config, parameters)
-            lightning_module = self.initialize_lightning_module(model)
-            self.train_model(lightning_module)
+        with Parallel(n_jobs=self.n_jobs) as executor:
+            while len(self.sampler.history) < self.budget:
+                self.worklist = []
+                models = []
+                self.tasklist = []
 
-            self.log_results(lightning_module)
+                while len(self.worklist) < self.n_jobs:
+                    try:
+                        parameters = self.sample()
+                        model = self.build_model(parameters)
+                        models.append(model)
+                        estimated_metrics, satisfied_bounds = self.estimate_metrics(model)
+                        current_num = len(self.tasklist) - 1
+                        self.tasklist.append(delayed(self.model_trainer.run_training(model, current_num, len(self.sampler.history) + current_num, self.config)))
+                    except Exception as e:
+                        print(str(e))
+
+                results = executor([task for task in self.tasklist])
+                for result, item in zip(results, self.worklist):
+                    parameters = item.parameters
+                    metrics = {**item.results, **result}
+                    for k, v in metrics.items():
+                        metrics[k] = float(v)
+
+                    self.sampler.tell_result(parameters, metrics)
+        # for i in range(self.budget):
+        #     self.global_num = i
+        #     self.setup_model_logging()
+        #     parameters = self.sample()
+        #     model = self.model_trainer.build_model(self.search_space, parameters)
+        #     lightning_module = self.initialize_lightning_module(model)
+        #     self.train_model(lightning_module)
+
+        #     self.log_results(lightning_module)
 
     def after_search(self):
-        self.extract_best_model()
+        pass
+        # self.extract_best_model()
 
     def build_model(self, parameters):
         try:
@@ -147,8 +168,24 @@ class DirectNAS(NASBase):
         trainer.fit(model)
 
     def sample(self):
-        parameters = self.sampler.sample()
+        parameters = self.sampler.next_parameters()
         return parameters
+
+        # FIXME: Integrate better intro current code
+    def estimate_metrics(self, model):
+        if self.predictor:
+            estimated_metrics = self.predictor.estimate(model)
+        else:
+            estimated_metrics = {}
+
+        satisfied_bounds = []
+        for k, v in estimated_metrics.items():
+            if k in self.bounds:
+                distance = v / self.bounds[k]
+                msglogger.info(f"{k}: {float(v):.8f} ({float(distance):.2f})")
+                satisfied_bounds.append(distance <= 1.2)
+
+        return estimated_metrics, satisfied_bounds
 
     def setup_model_logging(self):
         self.callbacks = common_callbacks(self.config)
@@ -177,25 +214,13 @@ class DirectNAS(NASBase):
 class AgingEvolutionNAS(DirectNAS):
     def __init__(self,
                  budget=2000,
-                 parametrization=None,
-                 bounds=None,
-                 random_state=None,
-                 population_size=100,
-                 presample=True,
                  n_jobs=10,
+                 presample=True,
                  *args, **kwargs) -> None:
         super().__init__(budget, *args, **kwargs)
-        self.random_state = np.random.RandomState()
-        self.parametrization = parametrization
-        self.bounds = bounds
-        self.population_size = population_size
         self.presample = presample
         self.worklist = []
         self.n_jobs=n_jobs
-
-    def sample(self):
-        parameters = self.sampler.next_parameters()
-        return parameters
 
     def append_to_worklist(self, parameters, estimated_metrics={}, satisfied_bounds=[]):
         worklist_item = WorklistItem(parameters, estimated_metrics)
@@ -205,31 +230,6 @@ class AgingEvolutionNAS(DirectNAS):
                 self.worklist.append(worklist_item)
         else:
             self.worklist.append(worklist_item)
-
-    # FIXME: Integrate better intro current code
-    def estimate_metrics(self, model):
-        if self.predictor:
-            estimated_metrics = self.predictor.estimate(model)
-        else:
-            estimated_metrics = {}
-
-        satisfied_bounds = []
-        for k, v in estimated_metrics.items():
-            if k in self.bounds:
-                distance = v / self.bounds[k]
-                msglogger.info(f"{k}: {float(v):.8f} ({float(distance):.2f})")
-                satisfied_bounds.append(distance <= 1.2)
-
-        return estimated_metrics, satisfied_bounds
-
-    def before_search(self):
-        self.initialize_dataset()
-        self.search_space = self.build_search_space()
-        parametrization = self.search_space.parametrization(flatten=True)
-        self.sampler = AgingEvolution(parametrization=parametrization,
-                                        bounds=self.bounds,
-                                        population_size=self.population_size,
-                                        random_state=self.random_state)
 
     def search(self):
         with Parallel(n_jobs=self.n_jobs) as executor:
