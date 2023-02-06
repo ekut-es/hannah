f6c7314956d4fc6de30231339c8362818f6ea5d5
Moritz Reiber <moritz.reiber@uni-tuebingen.de>
2023-02-06 14:41:21 +0100

    add symbolic casting

---
+++ hannah/nas/expressions/types.py
@@ -0,0 +1,18 @@
+from ..core.expression import Expression
+
+
+class Cast(Expression):
+    def __init__(self, expr, type) -> None:
+        super().__init__()
+        self.expr = expr
+        self.type = type
+
+    def evaluate(self):
+        return self.type(self.expr.evaluate())
+
+    def format(self, indent=2, length=80) -> str:
+        return f"{self.type}({self.expr})"
+
+class Int(Cast):
+    def __init__(self, expr) -> None:
+        super().__init__(expr, type=int)
