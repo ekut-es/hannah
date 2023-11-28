4458e312b61b31572969e20a7049ad1b56c47630
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2023-11-28 18:53:03 +0100

    Add kakao-resnet models

---
+++ hannah/models/kakao_resnet.py
@@ -0,0 +1,97 @@
+#
+# Copyright (c) 2023 Hannah contributors.
+#
+# This file is part of hannah.
+# See https://github.com/ekut-es/hannah for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import torch.nn as nn
+
+
+# Model (from KakaoBrain: https://github.com/wbaek/torchskeleton)
+class Mul(nn.Module):
+    def __init__(self, weight):
+        super(Mul, self).__init__()
+        self.weight = weight
+
+    def forward(self, x):
+        return x * self.weight
+
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+
+class Residual(nn.Module):
+    def __init__(self, module):
+        super(Residual, self).__init__()
+        self.module = module
+
+    def forward(self, x):
+        return x + self.module(x)
+
+
+def conv_bn(channels_in, channels_out, kernel_size=3, stride=1, padding=1, groups=1):
+    return nn.Sequential(
+        nn.Conv2d(
+            channels_in,
+            channels_out,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            groups=groups,
+            bias=False,
+        ),
+        nn.BatchNorm2d(channels_out),
+        nn.ReLU(inplace=True),
+    )
+
+
+def resnet8(*args, **kwargs):
+    num_class = 10
+    model = nn.Sequential(
+        conv_bn(3, 64, kernel_size=3, stride=1, padding=1),
+        conv_bn(64, 128, kernel_size=5, stride=2, padding=2),
+        Residual(nn.Sequential(conv_bn(128, 128), conv_bn(128, 128))),
+        conv_bn(128, 256, kernel_size=3, stride=1, padding=1),
+        nn.MaxPool2d(2),
+        Residual(nn.Sequential(conv_bn(256, 256), conv_bn(256, 256))),
+        conv_bn(256, 128, kernel_size=3, stride=1, padding=0),
+        nn.AdaptiveMaxPool2d((1, 1)),
+        Flatten(),
+        nn.Linear(128, num_class, bias=False),
+        Mul(0.2),
+    )
+
+    return model
+
+
+def resnet8_025(*args, **kwargs):
+    num_class = 10
+    model = nn.Sequential(
+        conv_bn(3, 16, kernel_size=4, stride=2, padding=0),
+        conv_bn(16, 32, kernel_size=5, stride=2, padding=2),
+        Residual(nn.Sequential(conv_bn(32, 32), conv_bn(32, 32))),
+        conv_bn(32, 64, kernel_size=3, stride=1, padding=1),
+        nn.MaxPool2d(2),
+        Residual(nn.Sequential(conv_bn(64, 64), conv_bn(64, 64))),
+        conv_bn(64, 32, kernel_size=3, stride=1, padding=0),
+        nn.AdaptiveMaxPool2d((1, 1)),
+        Flatten(),
+        nn.Linear(32, num_class, bias=False),
+        Mul(0.2),
+    )
+
+    return model
